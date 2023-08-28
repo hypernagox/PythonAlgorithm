dcd2e5e9d6f2094013557f3e8714194557abcaec
hypernagox <hypernagox@tukorea.ac.kr>
2023-08-28 21:31:34 +0900

    Aug 28 11404
    11404 플로이드

--- algorithm.py
+++ algorithm.py
@@ -1,30 +1,24 @@
 import sys
 input = sys.stdin.readline
-
-n,m = map(int,input().split())
-dists = [float("INF")] * n
-adj=[]
+n = int(input())
+m = int(input())
+dist = [[float("INF")] * n for _ in range(n) ]
+for i in range(n):
+    dist[i][i]=0
 for _ in range(m):
-    u, v, w = map(int,input().split())
-    adj.append((u - 1,v - 1,w))
-dists[0] = 0
-ok=True
-for _ in range(n-1):
-    for u,v,w in adj:
-        if float("INF") == dists[u]:continue
-        if(dists[v] > dists[u] + w):
-            dists[v] = dists[u] + w
-for u,v,w in adj:
-    if float("INF") == dists[u]: continue
-    if (dists[v] > dists[u] + w):
-        ok = False
-if ok:
-    for i in dists[1:]:
-        if float("INF") == i : print(-1)
-        else: print(i)
-else:
-    print(-1)
-
+    u,v,w = map(int,input().split())
+    dist[u-1][v-1] = min(dist[u-1][v-1],w)
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            dist[i][j] = min(dist[i][j],dist[i][k] + dist[k][j])
+for i in range(n):
+    for j in range(n):
+        if float("INF") == dist[i][j]:
+            print(0,end = " ")
+        else:
+            print(dist[i][j],end = " ")
+    print()
 
 
 

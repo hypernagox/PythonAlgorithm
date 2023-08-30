ed356e0e76fc4a5ef41cdbe0ab03ac0b314955d4
hypernagox <hypernagox@tukorea.ac.kr>
2023-08-30 15:54:51 +0900

    Aug 30 1197
    1197 최소스패닝트리 (크루스칼)

--- algorithm.py
+++ algorithm.py
@@ -1,24 +1,42 @@
 import sys
 input = sys.stdin.readline
-n = int(input())
-m = int(input())
-dist = [[float("INF")] * n for _ in range(n) ]
-for i in range(n):
-    dist[i][i]=0
+n,m = map(int,input().split())
+class DisjointSet:
+    def __init__(self,n):
+        self.parent=[i for i in range(n)]
+        self.rank = [0 for _ in range(n)]
+    def Find(self,n):
+        if self.parent[n] == n:
+            return n
+        self.parent[n] = self.Find(self.parent[n])
+        return self.parent[n]
+    def Union(self,u,v):
+        if u == v:
+            return False
+        u = self.Find(u)
+        v = self.Find(v)
+        if u == v :
+            return False
+        if(self.rank[u] > self.rank[v]):
+            u,v = v,u
+        self.parent[u] = v
+        if(self.rank[u] == self.rank[v]):
+            self.rank[v] +=1
+        return True
+adj = []
 for _ in range(m):
     u,v,w = map(int,input().split())
-    dist[u-1][v-1] = min(dist[u-1][v-1],w)
-for k in range(n):
-    for i in range(n):
-        for j in range(n):
-            dist[i][j] = min(dist[i][j],dist[i][k] + dist[k][j])
-for i in range(n):
-    for j in range(n):
-        if float("INF") == dist[i][j]:
-            print(0,end = " ")
-        else:
-            print(dist[i][j],end = " ")
-    print()
+    adj.append((u - 1,v - 1,w))
+adj.sort(key = lambda x:x[2])
+uf = DisjointSet(n)
+res = 0
+for u,v,w in adj:
+    if uf.Union(u,v):
+        res +=w
+print(res)
+
+
+
 
 
 

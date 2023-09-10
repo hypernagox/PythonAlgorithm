4c1ee5cd42898f7525134646a79364b1b936a091
hypernagox <128601833+hypernagox@users.noreply.github.com>
2023-09-10 17:29:30 +0900

    Sep 10 1854
    1854 K번째 최단경로

--- algorithm.py
+++ algorithm.py
@@ -1,42 +1,28 @@
+import heapq
 import sys
 input = sys.stdin.readline
-a,b,c,d = map(int,input().split())
-edges = [0] * d
-bestDists = [-float("INF")] * a
-visited = [False] * a
-adj = [[] for _ in range(a)]
-for i in range(d):
+n,m,k = map(int,input().split())
+adj = [[] for _ in range(n)]
+for _ in range(m):
     u,v,w = map(int,input().split())
-    edges[i] = (u,v,w)
-    adj[u].append(v)
-benefits = [int(x) for x in input().split()]
-bestDists[b] = benefits[b]
-res = benefits[b]
-def dfs(cur):
-    if cur == c :
-        return True
-    visited[cur] = True
-    can_visit = False
-    for next in adj[cur]:
-        if not visited[next]:
-            can_visit |= dfs(next)
-    return can_visit
-for _ in range(a-1):
-    for u,v,w in edges:
-        if -float("INF") == bestDists[u]:
-            continue
-        if bestDists[v] < bestDists[u] - w + benefits[v]:
-            bestDists[v] = bestDists[u] - w + benefits[v]
-if -float("INF") == bestDists[c]:
-    print("gg")
-    sys.exit()
-for u, v, w in edges:
-    if -float("INF") == bestDists[u]:
-        continue
-    if bestDists[v] < bestDists[u] - w + benefits[v]:
-        if dfs(v):
-            print("Gee")
-            sys.exit()
-        else:
-            visited = [False] * a
-print(bestDists[c])
+    adj[u-1].append((w,v-1))
+pq = []
+citys = [[] for _ in range(n)]
+heapq.heappush(pq,(0,0))
+citys[0].append(0)
+while pq:
+    dist,cur = heapq.heappop(pq)
+    for cost,next in adj[cur]:
+        nextDist = cost + dist
+        if len(citys[next]) < k :
+            heapq.heappush(pq,(nextDist,next))
+            heapq.heappush(citys[next],-nextDist)
+        elif citys[next][0] < -nextDist :
+            heapq.heappop(citys[next])
+            heapq.heappush(citys[next],-nextDist)
+            heapq.heappush(pq,(nextDist,next))
+for i in range(n):
+    if len(citys[i]) == k:
+        print(-citys[i][0])
+    else:
+        print(-1)

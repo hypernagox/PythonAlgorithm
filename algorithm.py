6050d291c434c88f9b7a3f445d4df880c1d34836
hypernagox <128601833+hypernagox@users.noreply.github.com>
2023-09-11 18:04:09 +0900

    Sep 11 11403
    11403번 경로찾기

--- algorithm.py
+++ algorithm.py
@@ -1,28 +1,24 @@
-import heapq
 import sys
 input = sys.stdin.readline
-n,m,k = map(int,input().split())
-adj = [[] for _ in range(n)]
-for _ in range(m):
-    u,v,w = map(int,input().split())
-    adj[u-1].append((w,v-1))
-pq = []
-citys = [[] for _ in range(n)]
-heapq.heappush(pq,(0,0))
-citys[0].append(0)
-while pq:
-    dist,cur = heapq.heappop(pq)
-    for cost,next in adj[cur]:
-        nextDist = cost + dist
-        if len(citys[next]) < k :
-            heapq.heappush(pq,(nextDist,next))
-            heapq.heappush(citys[next],-nextDist)
-        elif citys[next][0] < -nextDist :
-            heapq.heappop(citys[next])
-            heapq.heappush(citys[next],-nextDist)
-            heapq.heappush(pq,(nextDist,next))
+n = int(input())
+dists = [[sys.maxsize] * n for _ in range(n)]
 for i in range(n):
-    if len(citys[i]) == k:
-        print(-citys[i][0])
-    else:
-        print(-1)
+    cnt = 0
+    temp = list(map(int,input().split()))
+    for j in temp:
+        if 1 == j:
+            dists[i][cnt] = 1
+        cnt += 1
+for k in range(n):
+    for u in range(n):
+        for v in range(n):
+            dists[u][v] = min(dists[u][v],dists[u][k] + dists[k][v])
+for lists in dists:
+    for i in lists:
+        if sys.maxsize == i :
+            print(0,end = ' ')
+        else:
+            print(1,end=' ')
+    print()
+
+

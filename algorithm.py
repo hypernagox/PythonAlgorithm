6a0effbdbd7e6e4185f2a0266473c882398e30fc
hypernagox <128601833+hypernagox@users.noreply.github.com>
2023-09-12 11:35:15 +0900

    Sep 12 1414
    1414번 불우이웃돕기

--- algorithm.py
+++ algorithm.py
@@ -1,24 +1,45 @@
 import sys
 input = sys.stdin.readline
+import heapq
 n = int(input())
-dists = [[sys.maxsize] * n for _ in range(n)]
+adj = [[] for _ in range(n)]
+visited = [False] * n
+bestCost = [sys.maxsize] * n
+sum = 0
 for i in range(n):
-    cnt = 0
-    temp = list(map(int,input().split()))
-    for j in temp:
-        if 1 == j:
-            dists[i][cnt] = 1
-        cnt += 1
-for k in range(n):
-    for u in range(n):
-        for v in range(n):
-            dists[u][v] = min(dists[u][v],dists[u][k] + dists[k][v])
-for lists in dists:
-    for i in lists:
-        if sys.maxsize == i :
-            print(0,end = ' ')
+    temp = str(input().strip())
+    j = 0
+    for ch in temp:
+        x = 0
+        if '0' == ch:
+            x = 0
+        elif ch.islower():
+            x = ord(ch) - ord('a') + 1
         else:
-            print(1,end=' ')
-    print()
+            x = ord(ch) - ord('A') + 27
+        if i != j and 0 != x:
+            adj[i].append((x,j))
+            adj[j].append((x,i))
+        sum += x
+        j += 1
+pq = []
+bestCost[0] = 0
+res = 0
+heapq.heappush(pq,(0,0))
+while pq:
+    cost,cur = heapq.heappop(pq)
+    if visited[cur]:
+        continue
+    visited[cur] = True
+    res += cost
+    for dist,next in adj[cur]:
+        if not visited[next] and bestCost[next] > dist:
+            bestCost[next] = dist
+            heapq.heappush(pq,(dist,next))
+if all(visited):
+    print(sum - res)
+else:
+    print(-1)
+
 
 

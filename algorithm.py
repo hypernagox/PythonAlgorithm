0d2852d8102526132c44b14fc470121f5cd1f253
hypernagox <128601833+hypernagox@users.noreply.github.com>
2023-09-12 17:28:35 +0900

    Sep 12 1414
    1414 불우이웃돕기 (크루스칼)

--- algorithm.py
+++ algorithm.py
@@ -1,45 +1,55 @@
 import sys
 input = sys.stdin.readline
-import heapq
+sys.setrecursionlimit(100000)
+class DisjointSet:
+    def __init__(self,n):
+        self.parent = [ i for i in range(n)]
+        self.rank = [0] * n
+    def Find(self,u):
+        if self.parent[u] == u:
+            return u
+        self.parent[u] = self.Find(self.parent[u])
+        return self.parent[u]
+    def Union(self,u,v):
+        u = self.Find(u)
+        v = self.Find(v)
+        if u == v:
+            return False
+        if self.rank[u] > self.rank[v]:
+            u,v=v,u
+        self.parent[u] = v
+        if self.rank[u] == self.rank[v]:
+            self.rank[v] += 1
+        return True
 n = int(input())
-adj = [[] for _ in range(n)]
-visited = [False] * n
-bestCost = [sys.maxsize] * n
+uf = DisjointSet(n)
+Edges = []
 sum = 0
 for i in range(n):
-    temp = str(input().strip())
-    j = 0
-    for ch in temp:
+    temp = input().strip()
+    for j in range(n):
+        if temp[j] == '0':
+            continue
         x = 0
-        if '0' == ch:
-            x = 0
-        elif ch.islower():
-            x = ord(ch) - ord('a') + 1
+        if temp[j].isupper():
+            x = ord(temp[j]) - ord('A') + 27
         else:
-            x = ord(ch) - ord('A') + 27
-        if i != j and 0 != x:
-            adj[i].append((x,j))
-            adj[j].append((x,i))
+            x = ord(temp[j]) - ord('a') + 1
         sum += x
-        j += 1
-pq = []
-bestCost[0] = 0
+        if i != j:
+           Edges.append((x,i,j))
+Edges.sort()
 res = 0
-heapq.heappush(pq,(0,0))
-while pq:
-    cost,cur = heapq.heappop(pq)
-    if visited[cur]:
-        continue
-    visited[cur] = True
-    res += cost
-    for dist,next in adj[cur]:
-        if not visited[next] and bestCost[next] > dist:
-            bestCost[next] = dist
-            heapq.heappush(pq,(dist,next))
-if all(visited):
+cnt = 0
+for w,u,v in Edges:
+    if uf.Union(u,v):
+        res += w
+        cnt += 1
+    if n-1 == cnt:
+        break
+if n-1 == cnt:
     print(sum - res)
 else:
     print(-1)
-
 
 

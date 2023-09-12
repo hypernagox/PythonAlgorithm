540f15612987f36dba8a61d292c587bd7d7e173e
hypernagox <128601833+hypernagox@users.noreply.github.com>
2023-09-12 17:52:03 +0900

    Sep 12 1389
    1389번 케빈베이컨의 6단계 법칙 (BFS)

--- algorithm.py
+++ algorithm.py
@@ -1,55 +1,33 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
-sys.setrecursionlimit(100000)
-class DisjointSet:
-    def __init__(self,n):
-        self.parent = [ i for i in range(n)]
-        self.rank = [0] * n
-    def Find(self,u):
-        if self.parent[u] == u:
-            return u
-        self.parent[u] = self.Find(self.parent[u])
-        return self.parent[u]
-    def Union(self,u,v):
-        u = self.Find(u)
-        v = self.Find(v)
-        if u == v:
-            return False
-        if self.rank[u] > self.rank[v]:
-            u,v=v,u
-        self.parent[u] = v
-        if self.rank[u] == self.rank[v]:
-            self.rank[v] += 1
-        return True
-n = int(input())
-uf = DisjointSet(n)
-Edges = []
-sum = 0
+n,m = map(int,input().split())
+adj = [[] for _ in range(n)]
+visited = [0] * n
+for _ in range(m):
+    u,v = map(int,input().split())
+    adj[u-1].append(v-1)
+    adj[v-1].append(u-1)
+def bfs (u):
+    pq = deque([u])
+    while pq:
+        cur = pq.popleft()
+        for next in adj[cur]:
+            if visited[next] == 0:
+                visited[next] = visited[cur] + 1
+                pq.append(next)
+    return sum(visited)
+res = [0] * n
+minval = sys.maxsize
 for i in range(n):
-    temp = input().strip()
-    for j in range(n):
-        if temp[j] == '0':
-            continue
-        x = 0
-        if temp[j].isupper():
-            x = ord(temp[j]) - ord('A') + 27
-        else:
-            x = ord(temp[j]) - ord('a') + 1
-        sum += x
-        if i != j:
-           Edges.append((x,i,j))
-Edges.sort()
-res = 0
-cnt = 0
-for w,u,v in Edges:
-    if uf.Union(u,v):
-        res += w
-        cnt += 1
-    if n-1 == cnt:
-        break
-if n-1 == cnt:
-    print(sum - res)
-else:
-    print(-1)
+    res[i] = bfs(i)
+    minval = min(minval,res[i])
+    visited = [0] * n
+for i in range(n):
+    if res[i] == minval:
+        print(i + 1)
+        exit()
+
+
 
 

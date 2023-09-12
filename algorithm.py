188e2369212b06ff989a9a75549655861a643a5a
hypernagox <128601833+hypernagox@users.noreply.github.com>
2023-09-12 18:03:51 +0900

    Sep 3 1389
    1389번 케빈베이컨의 6단계 법칙 (플로이드-워셜)

--- algorithm.py
+++ algorithm.py
@@ -1,33 +1,22 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 n,m = map(int,input().split())
-adj = [[] for _ in range(n)]
-visited = [0] * n
+dists = [[sys.maxsize] * n for _ in range(n)]
 for _ in range(m):
     u,v = map(int,input().split())
-    adj[u-1].append(v-1)
-    adj[v-1].append(u-1)
-def bfs (u):
-    pq = deque([u])
-    while pq:
-        cur = pq.popleft()
-        for next in adj[cur]:
-            if visited[next] == 0:
-                visited[next] = visited[cur] + 1
-                pq.append(next)
-    return sum(visited)
-res = [0] * n
+    dists[u-1][v-1] = dists[v-1][u-1] = 1
+for k in range(n):
+    for u in range(n):
+        for v in range(n):
+            dists[u][v] = min(dists[u][v],dists[u][k] + dists[k][v])
 minval = sys.maxsize
+res = [0] * n
 for i in range(n):
-    res[i] = bfs(i)
+    res[i] = sum(dists[i])
     minval = min(minval,res[i])
-    visited = [0] * n
 for i in range(n):
-    if res[i] == minval:
-        print(i + 1)
+    if minval == res[i]:
+        print(i+1)
         exit()
-
-
 
 

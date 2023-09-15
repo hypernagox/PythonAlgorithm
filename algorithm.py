e7ef876cbd628260bef3c7af064e922608f06111
hypernagox <128601833+hypernagox@users.noreply.github.com>
2023-09-15 17:53:47 +0900

    Sep 15 1068
    1068 트리

--- algorithm.py
+++ algorithm.py
@@ -1,21 +1,25 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(1000000)
-N = int(input())
-tree = [[] for _ in range(N)]
-for _ in range(N - 1):
-    a,b = map(int,input().split())
-    tree[a-1].append(b-1)
-    tree[b-1].append(a-1)
-visited = [False] * N
-parent = [0] * N
-def dfs(n):
-    visited[n] = True
-    for next in tree[n]:
-        if visited[next]:
-            continue
-        parent[next] = n
+sys.setrecursionlimit(10000000)
+n = int(input())
+tree = [[] for _ in range(n)]
+temp = list(map(int,input().split()))
+root = 0
+for i in range(n):
+    if -1 == temp[i]:
+        root = i
+        continue
+    tree[temp[i]].append(i)
+m = int(input())
+cnt = 0
+def dfs(cur):
+    if m == cur:
+       return
+    global cnt
+    for next in tree[cur]:
         dfs(next)
-dfs(0)
-for i in range(1,N):
-    print(parent[i] + 1)
+    if len(tree[cur]) == 0 or (len(tree[cur]) == 1 and tree[cur][0] == m):
+        cnt += 1
+dfs(root)
+print(cnt)
+

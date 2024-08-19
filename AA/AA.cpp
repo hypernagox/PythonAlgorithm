b4347b644dd1185069fcb29a2848833d0dca33cb
hypernagox <hypernagox@tukorea.ac.kr>
2024-08-20 07:15:17 +0900

    누군가 "어떤애" 보다 선행되어야 한다면 그래프 관계는 누군가 -> 어떤애 형식임

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -1,67 +1,32 @@
 #include <iostream>
 #include <algorithm>
-#include <unordered_set>
 #include <vector>
+#include <queue>
+#include <ranges>
 using namespace std;
-
-class UnionFind
+vector<int> v[32001];
+bool visited[32001];
+vector<int> res;
+void dfs(int n)
 {
-public:
-	vector<int> parents;
-	vector<int> rank;
-	
-	UnionFind(int n) :rank(n),parents(n) { for (int i = 0; i < n; ++i)parents[i] = i; }
-
-	void Union(int u,int v){
-		int uf = Find(u);
-		int vf = Find(v);
-		if (u == v || uf == vf)return;
-		if (rank[uf] > rank[vf])
-			swap(uf, vf);
-		parents[vf] = uf;
-		if (rank[uf] == rank[vf])
-			++rank[vf];
+	if (visited[n])return;
+	visited[n] = true;
+	for (auto i : v[n]) {
+		if (!visited[i])dfs(i);
 	}
-	int Find(int u){
-		return parents[u] = parents[u] == u ? u : Find(parents[u]);
-	}
-};
+	res.emplace_back(n);
+}
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	int n, m, k;
-	cin >> n >> m >> k;
-	vector<int> party[50];
-	unordered_set<int> tp;
-	tp.reserve(50);
-	UnionFind uf{ n };
-	while (k--) {
-		int x; cin >> x;
-		tp.emplace(x - 1);
-		uf.rank[x - 1] = -50;
+	int n, m;
+	cin >> n >> m;
+	res.reserve(n);
+	while (m--) {
+		int x, y; cin >> x >> y;
+		v[x].emplace_back(y);
 	}
-	for (int i = 0; i < m; ++i) {
-		int x; cin >> x;
-		for (int j = 0; j < x; ++j) {
-			int y; cin >> y;
-			party[i].emplace_back(y);
-		}
-		int parent = party[i][0] - 1;
-		for (int k = 1; k < x; ++k) {
-			uf.Union(parent, party[i][k] - 1);
-		}
-	}
-	int res = 0;
-	for (int i = 0; i < m; ++i) {
-		bool flag = true;
-		for (const auto p : party[i]) {
-			if (tp.contains(uf.Find(p - 1))) {
-				flag = false;
-				break;
-			}
-		}
-		if (flag)++res;
-	}
-	cout << res;
+	for (int i = 1; i <= n; ++i)dfs(i);
+	for (auto i : res | views::reverse)cout << i << ' ';
 }

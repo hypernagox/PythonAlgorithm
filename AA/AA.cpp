edd9c0b7e4fea2899e2bbde7a23ba36746b49bd2
hypernagox <hypernagox@tukorea.ac.kr>
2024-08-19 20:42:26 +0900

    왼쪽 전깃줄을 기준으로 오름차순 정렬하고 오른쪽 전깃줄의 최대 증가수열로 구한 수열이 곧 선택된 전깃줄이다 (자르지 않은)

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+struct Line {
+	uint16_t l, r;
+	uint16_t v;
+};
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	int dp[501];
-	int arr[13];
-	int n, m;
+	Line l[100];
+	int n;
 	cin >> n;
-	for (int i = 1; i <= n; ++i)cin >> arr[i];
-	cin >> m;
-	for (auto& i : dp)i = 54321;
-	dp[0] = 0;
-	for (int i = 1; i <= n; ++i) {
-		for (int j = arr[i]; j <= m; ++j) {
-			dp[j] = min(dp[j], dp[j - arr[i]] + 1);
+	for (int i = 0; i < n; ++i)cin >> l[i].l >> l[i].r;
+	ranges::sort(l, l + n, {}, &Line::l);
+	int res = 1;
+	l[0].v = 1;
+	for (int i = 1; i < n; ++i) {
+		int max_val = 1;
+		for (int j = i - 1; j >= 0; --j) {
+			if (l[j].r < l[i].r && l[j].v  + 1> max_val) {
+				max_val = l[j].v + 1;
+			}
 		}
+		l[i].v = max_val;
+		res = max(res, max_val);
 	}
-	cout << dp[m];
+	cout << n - res;
 }

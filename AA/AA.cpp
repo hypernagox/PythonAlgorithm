94e728cc134ecd45b51a1472b92944adba66e5c6
hypernagox <hypernagox@tukorea.ac.kr>
2024-08-19 18:02:14 +0900

    1차원 배낭 풀이

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -1,35 +1,21 @@
 #include <iostream>
 #include <algorithm>
-#include <ranges>
-#include <vector>
 using namespace std;
-
-int dp[20][20];
-int arr[20][20];
-
-int go(int i,int j)
-{
-	if (0 > i || 0 > j)return 54321;
-	if (dp[i][j])return dp[i][j];
-	return dp[i][j] = min(go(i - 1, j), go(i, j - 1)) + arr[i][j];
-}
-
 int main()
 {
+	int dp[100001]{};
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	int n;
-	cin >> n;
+	int n, m;
+	int res = 0;
+	cin >> n >> m;
 	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
-			cin >> arr[i][j];
+		int w, v;
+		cin >> w >> v;
+		for (int j = m; j >= w; --j) {
+			dp[j] = max(dp[j], dp[j - w] + v);
+			res = max(dp[j], res);
 		}
 	}
-	dp[0][0] = arr[0][0];
-	//for (int i = 1; i < n; ++i) {
-	//	dp[0][i] = dp[0][i - 1] + arr[0][i];
-	//	dp[i][0] = dp[i - 1][0] + arr[i][0];
-	//}
-	go(n - 1, n - 1);
-	cout << dp[n - 1][n - 1];
+	cout << res;
 }

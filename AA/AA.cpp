389551721e871baaccf55d647ebc3ac16aca6c33
hypernagox <hypernagox@tukorea.ac.kr>
2024-08-19 13:25:25 +0900

    이거 초기화 미리 안했었으면 경계 넘어가는 값은 INF를 반환해야 정상작동함

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -9,7 +9,7 @@ int arr[20][20];
 
 int go(int i,int j)
 {
-	if (0 > i || 0 > j)return 0;
+	if (0 > i || 0 > j)return 54321;
 	if (dp[i][j])return dp[i][j];
 	return dp[i][j] = min(go(i - 1, j), go(i, j - 1)) + arr[i][j];
 }
@@ -26,10 +26,10 @@ int main()
 		}
 	}
 	dp[0][0] = arr[0][0];
-	for (int i = 1; i < n; ++i) {
-		dp[0][i] = dp[0][i - 1] + arr[0][i];
-		dp[i][0] = dp[i - 1][0] + arr[i][0];
-	}
+	//for (int i = 1; i < n; ++i) {
+	//	dp[0][i] = dp[0][i - 1] + arr[0][i];
+	//	dp[i][0] = dp[i - 1][0] + arr[i][0];
+	//}
 	go(n - 1, n - 1);
 	cout << dp[n - 1][n - 1];
 }

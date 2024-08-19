31b266420e14360ff61de46384297a83c3ba7913
hypernagox <hypernagox@tukorea.ac.kr>
2024-08-20 06:29:11 +0900

    1. 그냥 대충 대표노드 아무나 정하고 순서대로 유니온 , 2. 비록 대표노드가 진실을 아는 사람이 아니게 되어도 진실배열 순회하면서 연결관계인지 파악한다.

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -1,30 +1,68 @@
 #include <iostream>
 #include <algorithm>
+#include <unordered_set>
+#include <vector>
 using namespace std;
-struct Line {
-	uint16_t l, r;
-	uint16_t v;
+
+class UnionFind
+{
+public:
+	vector<int> parents;
+	vector<int> rank;
+	
+	UnionFind(int n) :rank(n),parents(n) { for (int i = 0; i < n; ++i)parents[i] = i; }
+
+	void Union(int u,int v){
+		int uf = Find(u);
+		int vf = Find(v);
+		if (u == v || uf == vf)return;
+		if (rank[uf] > rank[vf])
+			swap(uf, vf);
+		parents[vf] = uf;
+		if (rank[uf] == rank[vf])
+			++rank[vf];
+	}
+	int Find(int u){
+		return parents[u] = parents[u] == u ? u : Find(parents[u]);
+	}
+	bool IsUnion(int u, int v){ return Find(u) == Find(v); }
 };
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	Line l[100];
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; ++i)cin >> l[i].l >> l[i].r;
-	ranges::sort(l, l + n, {}, &Line::l);
-	int res = 1;
-	l[0].v = 1;
-	for (int i = 1; i < n; ++i) {
-		int max_val = 1;
-		for (int j = i - 1; j >= 0; --j) {
-			if (l[j].r < l[i].r && l[j].v  + 1> max_val) {
-				max_val = l[j].v + 1;
+	int n, m, k;
+	cin >> n >> m >> k;
+	vector<int> party[50];
+	vector<int> tp;
+	tp.reserve(50);
+	UnionFind uf{ n };
+	while (k--) {
+		int x; cin >> x;
+		tp.emplace_back(x - 1);
+	}
+	for (int i = 0; i < m; ++i) {
+		int x; cin >> x;
+		for (int j = 0; j < x; ++j) {
+			int y; cin >> y;
+			party[i].emplace_back(y);
+		}
+		int parent = party[i][0] - 1;
+		for (int k = 1; k < x; ++k) {
+			uf.Union(parent, party[i][k] - 1);
+		}
+	}
+	int res = 0;
+	for (int i = 0; i < m; ++i) {
+		bool flag = true;
+		for (const auto p : party[i]) {
+			for(const auto t:tp)
+			if (uf.IsUnion(p-1,t)) {
+				flag = false;
+				break;
 			}
 		}
-		l[i].v = max_val;
-		res = max(res, max_val);
+		if (flag)++res;
 	}
-	cout << n - res;
+	cout << res;
 }

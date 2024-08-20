f96bd0460afeef894c196a98bf5954c8075e059f
hypernagox <hypernagox@tukorea.ac.kr>
2024-08-20 12:00:02 +0900

    값 그 자체를 인덱스로 써버리면 누가 오버라이트한다 .

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -1,51 +1,43 @@
 #include <iostream>
 #include <algorithm>
+#include <ranges>
 #include <vector>
-#include <queue>
 using namespace std;
-vector<pair<int, int>> adj[10001];
-vector<pair<int, int>> adj2[10001];
-int inDegree[10001];
-queue<int> q;
-int res[10001];
-int cnt;
-int u, v;
-bool visited[10001];
+pair<int, int> dp[1000];
+int dp2[1000]{};
+
+void go(int n)
+{
+	if (dp2[n] != n)
+		go(dp2[n]);
+	cout << dp[n].first << '\n';
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	int n, m; cin >> n >> m;
-	int M = m;
-	while (M--) {
-		int x, y, z; cin >> x >> y >> z;
-		adj[x].emplace_back(y, z);
-		adj2[y].emplace_back(x, z);
-		++inDegree[y];
-	}
-	for (int i = 1; i <= n; ++i) {
-		if (0 == inDegree[i])q.emplace(i);
-	}
-	while (!q.empty()) {
-		const int node = q.front(); q.pop();
-		for (const auto [n, val] : adj[node]) {
-			if (0 == --inDegree[n])q.emplace(n);
-			res[n] = max(res[n], res[node] + val);
-		}
-	}
-	cin >> u >> v;
-	cout << res[v] << '\n';
-	q.emplace(v);
-	while (!q.empty()) {
-		const auto n = q.front(); q.pop();
-		visited[n] = true;
-		for (auto i : adj2[n]) {
-			if (res[i.first] == res[n] - i.second) {
-				++cnt;
-				if(!visited[i.first])
-					q.emplace(i.first);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; ++i) { cin >> dp[i].first; dp2[i] = i; }
+	dp[0].second = 1;
+	int res = 1;
+	int max_idx = 0;
+	for (int i = 0; i < n; ++i) {
+		int max_v = 1;
+		for (int j = i ; j >= 0; --j) {
+			if (dp[j].first < dp[i].first && dp[j].second + 1 > max_v) {
+				dp2[i] = j;
+				max_v = dp[j].second + 1;
 			}
 		}
+		dp[i].second = max_v;
+		if (res < max_v)
+		{
+			max_idx = i;
+			res = max_v;
+		}
 	}
-	cout << cnt;
+	cout << res << '\n';
+	go(max_idx);
 }

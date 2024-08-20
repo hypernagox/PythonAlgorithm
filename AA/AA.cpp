8a0916691edb720c29a67889eefcc442c06bc8d0
hypernagox <hypernagox@tukorea.ac.kr>
2024-08-20 15:46:32 +0900

    그냥 냅색 복습

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -2,42 +2,23 @@
 #include <algorithm>
 #include <ranges>
 #include <vector>
+#include <map>
 using namespace std;
-pair<int, int> dp[1000];
-int dp2[1000]{};
-
-void go(int n)
-{
-	if (dp2[n] != n)
-		go(dp2[n]);
-	cout << dp[n].first << '\n';
-}
-
+int dp[101][1001];
+pair<int, int> p[101];
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; ++i) { cin >> dp[i].first; dp2[i] = i; }
-	dp[0].second = 1;
-	int res = 1;
-	int max_idx = 0;
-	for (int i = 0; i < n; ++i) {
-		int max_v = 1;
-		for (int j = i ; j >= 0; --j) {
-			if (dp[j].first < dp[i].first && dp[j].second + 1 > max_v) {
-				dp2[i] = j;
-				max_v = dp[j].second + 1;
-			}
-		}
-		dp[i].second = max_v;
-		if (res < max_v)
-		{
-			max_idx = i;
-			res = max_v;
+	int n, m; cin >> n >> m;
+	for (int i = 1; i <= n; ++i)cin >> p[i].first >> p[i].second;
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <= m; ++j) {
+			if (p[i].second <= j)
+				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - p[i].second] + p[i].first);
+			else
+				dp[i][j] = dp[i - 1][j];
 		}
 	}
-	cout << res << '\n';
-	go(max_idx);
+	cout << dp[n][m];
 }

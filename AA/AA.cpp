b08e21862e6530918da206a315e353efedbac95b
hypernagox <hypernagox@tukorea.ac.kr>
2024-08-22 16:25:33 +0900

    점화식 i - 2에 이전 결과가 포함되어있다.

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -21,10 +21,9 @@ int main()
 		}
 		dp[0][1] = arr[0][1];
 		dp[1][1] = arr[1][1];
-		
 		for (int i = 2; i <= n; ++i) {
-			dp[0][i] = max(dp[1][i - 1] + arr[0][i], max(dp[1][i - 2], dp[0][i - 2]) + arr[0][i]);
-			dp[1][i] = max(dp[0][i - 1] + arr[1][i], max(dp[0][i - 2], dp[1][i - 2]) + arr[1][i]);
+			dp[0][i] = max(dp[1][i - 1], dp[1][i - 2]) + arr[0][i];
+			dp[1][i] = max(dp[0][i - 1], dp[0][i - 2]) + arr[1][i];
 		}
 		cout << max(dp[0][n], dp[1][n]) << '\n';
 	}

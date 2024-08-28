740f69f4ae1c6f7fcb5df3b9c5360d734e214481
hypernagox <hypernagox@tukorea.ac.kr>
2024-08-28 10:15:45 +0900

    보석도둑

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -1,29 +1,27 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
+#include <set>
 using namespace std;
-struct Mat {
-	int r, c;
-};
-int dp[500][500];
-Mat m[500];
+struct Item { int v, m; };
+Item item[300000];
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	int n; cin >> n;
-	for (int i = 0; i < n; ++i) {
-		Mat mat; cin >> mat.r >> mat.c;
-		m[i] = mat;
-	}
-	for (int i = 1; i < n; ++i) {
-		for (int dy = 0; dy + i < n; ++dy) {
-			const int dx = dy + i;
-			dp[dy][dx] = 1 << 30;
-			for (int k = dy; k < dx; ++k) {
-				dp[dy][dx] = min(dp[dy][dx], dp[dy][k] + dp[k+1][dx] + m[dy].r * m[k].c * m[dx].c);
-			}
+	int n, k; cin >> n >> k;
+	uint64_t res = 0;
+	multiset<int> ms;
+	for (int i = 0; i < n; ++i)cin >> item[i].m >> item[i].v;
+	for (int i = 0; i < k; ++i) { int w; cin >> w; ms.emplace(w); }
+	ranges::sort(item, item + n, greater{}, &Item::v);
+	int cnt = k;
+	for (int i = 0; cnt && i < n; ++i) {
+		const auto iter = ms.lower_bound(item[i].m);
+		if (ms.end() != iter) {
+			ms.erase(iter);
+			res += item[i].v;
+			--cnt;
 		}
 	}
-	cout << dp[0][n - 1];
+	cout << res;
 }

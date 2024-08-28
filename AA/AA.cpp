e4a85b685c2fdd636cc79d236fa4f06cc90cbf4b
hypernagox <hypernagox@tukorea.ac.kr>
2024-08-28 10:58:24 +0900

    우선순위큐 풀이

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
 #include <algorithm>
-#include <set>
+#include <queue>
 using namespace std;
-struct Item { int v, m; };
+struct Item { 
+	int v, m;
+	auto operator<=>(const Item&)const noexcept = default;
+};
 Item item[300000];
+int bag[300000];
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	int n, k; cin >> n >> k;
 	uint64_t res = 0;
-	multiset<int> ms;
 	for (int i = 0; i < n; ++i)cin >> item[i].m >> item[i].v;
-	for (int i = 0; i < k; ++i) { int w; cin >> w; ms.emplace(w); }
-	ranges::sort(item, item + n, greater{}, &Item::v);
-	int cnt = k;
-	for (int i = 0; cnt && i < n; ++i) {
-		const auto iter = ms.lower_bound(item[i].m);
-		if (ms.end() != iter) {
-			ms.erase(iter);
-			res += item[i].v;
-			--cnt;
+	for (int i = 0; i < k; ++i) cin >> bag[i];
+	ranges::sort(item, item + n, {}, &Item::m);
+	ranges::sort(bag, bag + k);
+	priority_queue<Item> pq;
+	int idx = 0;
+	for (int i = 0; i < k; ++i) {
+		while (idx <= n && item[idx].m <= bag[i]) {
+			pq.emplace(item[idx++]);
+		}
+		if (!pq.empty()) {
+			res += pq.top().v;
+			pq.pop();
 		}
 	}
 	cout << res;

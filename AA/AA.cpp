0a22662dfd0d8c8410a61d858f1137adb5e7a715
hypernagox <hypernagox@tukorea.ac.kr>
2024-08-29 22:11:37 +0900

    세 용액 이분탐색 풀이

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 #include <ranges>
 using namespace std;
-int arr[100000];
+long long arr[5001];
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -11,21 +10,22 @@ int main()
     int n; cin >> n;
     for (int i = 0; i < n; ++i)cin >> arr[i];
     ranges::sort(arr, arr + n);
-    int s = 0;
-    int e = n - 1;
-    pair<int, int> res;
-    long long sum = 99876543210;
-    while (s < e) {
-        const int temp = arr[s] + arr[e];
-        const int abstemp = abs(temp);
-        if (sum > abstemp) {
-            sum = abstemp;
-            res = { arr[s],arr[e] };
+    arr[n] = INT64_MAX / 2;
+    long long res[3] = { arr[0],arr[1],arr[2] };
+    uint64_t sum = UINT64_MAX;
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = i + 1; j < n - 1; ++j) {
+            const long long temp = arr[i] + arr[j];
+            auto iter = ranges::lower_bound(arr + j + 1, arr + n, -(temp));
+            if (iter == arr+n || abs(*(iter - 1) + temp) <= abs(*iter + temp))
+                if(iter != arr + j + 1)
+                    --iter;
+            const uint64_t val = abs(temp + *iter);
+            if (sum > val) {
+                sum = val;
+                tie(res[0], res[1], res[2]) = { arr[i], arr[j], *iter };
+            }
         }
-        if (temp > 0)
-            --e;
-        else
-            ++s;
     }
-    cout << res.first << ' ' << res.second;
+    for (const auto i : res)cout << i << ' ';
 }

6ac7c7dd664554f94adfaab27a965b68bab25047
hypernagox <hypernagox@tukorea.ac.kr>
2024-08-30 18:39:02 +0900

    냅색 분할

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -1,31 +1,29 @@
 #include <iostream>
 #include <algorithm>
 #include <ranges>
+#include <vector>
 using namespace std;
-int arr[3][1000];
+int dp[10001];
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    int n; cin >> n;
-    for (int i = 0; i < n; ++i)cin >> arr[0][i] >> arr[1][i] >> arr[2][i];
-    int ans = 1 << 30;
-    for (int i = 0; i < 3; ++i) {
-        int dp[3][1000]{};
-        for (int j = 0; j < 3; ++j) {
-            if (i == j)dp[j][0] = arr[i][0];
-            else dp[j][0] = 1 << 30;
-        }
-        dp[i][n - 1] = 1000 * 1000 + 1;
-        for (int k = 1; k < n; ++k) {
-            dp[0][k] = min(dp[1][k - 1], dp[2][k - 1]) + arr[0][k];
-            dp[1][k] = min(dp[0][k - 1], dp[2][k - 1]) + arr[1][k];
-            dp[2][k] = min(dp[1][k - 1], dp[0][k - 1]) + arr[2][k];
-        }
-        for (int v = 0; v < 3; ++v) {
-            if (i == v)continue;
-            ans = min(ans, dp[v][n - 1]);
+    int n, m; cin >> n >> m;
+    vector<pair<int, int>> items;
+    for (int i = 0; i < n; ++i) {
+        int v, c, k; cin >> v >> c >> k;
+        for (int j = k; k >= 1; k = k >> 1) {
+            j = k - (k >> 1);
+            items.emplace_back(v * j, c * j);
         }
     }
-    cout << ans;
+    const int num = (int)items.size();
+    int res = -1;
+    for (int i = 0; i < num; ++i) {
+        for (int j = m; j >= items[i].first; --j) {
+            dp[j] = max(dp[j], dp[j - items[i].first] + items[i].second);
+            res = max(res, dp[j]);
+        }
+    }
+    cout << res;
 }

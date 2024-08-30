d93432c3b8f0bad4703877ea4e8e041bab55d6fe
hypernagox <hypernagox@tukorea.ac.kr>
2024-08-30 09:49:59 +0900

    투포인터

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -2,30 +2,26 @@
 #include <algorithm>
 #include <ranges>
 using namespace std;
-long long arr[5001];
+int arr[100000];
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    int n; cin >> n;
+    int n, m; cin >> n >> m;
     for (int i = 0; i < n; ++i)cin >> arr[i];
-    ranges::sort(arr, arr + n);
-    arr[n] = INT64_MAX / 2;
-    long long res[3] = { arr[0],arr[1],arr[2] };
-    uint64_t sum = UINT64_MAX;
-    for (int i = 0; i < n - 1; ++i) {
-        for (int j = i + 1; j < n - 1; ++j) {
-            const long long temp = arr[i] + arr[j];
-            auto iter = ranges::lower_bound(arr + j + 1, arr + n, -(temp));
-            if (iter == arr+n || abs(*(iter - 1) + temp) <= abs(*iter + temp))
-                if(iter != arr + j + 1)
-                    --iter;
-            const uint64_t val = abs(temp + *iter);
-            if (sum > val) {
-                sum = val;
-                tie(res[0], res[1], res[2]) = { arr[i], arr[j], *iter };
-            }
+    int res = 1 << 30;
+    int sum = 0;
+    int s = 0;
+    for (int i = 0; i < n; ++i) {
+        sum += arr[i];
+        while (sum >= m) {
+            sum -= arr[s];
+            res = min(res, i - s + 1);
+            ++s;
         }
     }
-    for (const auto i : res)cout << i << ' ';
+    if (res == 1 << 30)
+        cout << 0;
+    else
+        cout << res;
 }

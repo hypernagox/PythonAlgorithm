9813cfb57dfde49e730d8fac1b8c6413766ab588
hypernagox <hypernagox@tukorea.ac.kr>
2024-09-01 07:37:24 +0900

    사이클 판별 + 대상

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -2,37 +2,39 @@
 #include <algorithm>
 #include <ranges>
 #include <vector>
-#include <queue>
 using namespace std;
-vector<int> adj[1001];
-int costs[1001];
-int inDegree[1001];
-int res[1001];
+int arr[100001];
+bool visited[100001];
+bool done[100001];
+int res;
+void go(const int n)
+{
+    if (visited[n]) {
+        if (!done[n])
+        {
+            for (int i = arr[n]; i != n; i = arr[i])
+                ++res;
+            ++res;
+        }
+        return;
+    }
+    visited[n] = 1;
+    go(arr[n]);
+    done[n] = 1;
+}
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    int t, n, k; cin >> t;
+    int t, n; cin >> t;
     while (t--) {
-        cin >> n >> k;
-        for (int i = 1; i <= n; ++i) { cin >> costs[i]; inDegree[i] = 0; res[i] = costs[i]; adj[i].clear(); }
-        while (k--) {
-            int a, b; cin >> a >> b;
-            adj[a].emplace_back(b);
-            ++inDegree[b];
-        }
-        queue<int> q;
+        res = 0;
+        cin >> n;
+        for (int i = 1; i <= n; ++i) { cin >> arr[i]; visited[i] = done[i] = 0; }
         for (int i = 1; i <= n; ++i) {
-            if (!inDegree[i])q.emplace(i);
+            if (visited[i])continue;
+            go(i);
         }
-        int dest; cin >> dest;
-        while (!q.empty()) {
-            const auto node = q.front(); q.pop();
-            for (const auto i : adj[node]) {
-                if (0 == --inDegree[i])q.emplace(i);
-                res[i] = max(res[i], res[node] + costs[i]);
-            }
-        }
-        cout << res[dest] << '\n';
+        cout << n - res << '\n';
     }
 }

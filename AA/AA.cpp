f60bc38ac7e94ecf3b93f1cc326bc635c6c69f31
hypernagox <128601833+hypernagox@users.noreply.github.com>
2024-09-08 17:34:45 +0900

    외판원순회

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -1,31 +1,36 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
-int inOrder[100001];
-int postOrder[100001];
-int inOrderMap[100001];
-void findPreOrder(int inStart, int inEnd, int postStart, int postEnd) noexcept
+int dp[16][1 << 16];
+int g[16][16];
+uint16_t n;
+uint16_t DONE;
+int go(uint16_t cur, uint16_t visited)
 {
-    if (inStart > inEnd || postStart > postEnd) return;
-    int root = postOrder[postEnd];
-    cout << root << ' ';
-    int inRootIndex = inOrderMap[root];
-    int leftSubtreeSize = inRootIndex - inStart;
-    findPreOrder(inStart, inRootIndex - 1, postStart, postStart + leftSubtreeSize - 1);
-    findPreOrder(inRootIndex + 1, inEnd, postStart + leftSubtreeSize, postEnd - 1);
+    if (visited == DONE) return g[cur][0];
+    if (dp[cur][visited])return dp[cur][visited];
+    dp[cur][visited] = 987654321;
+    for (int i = 0; i < n; ++i) {
+        if (g[cur][i] == 987654321)continue;
+        if ((visited & (1 << i)) == (1 << i))continue;
+        dp[cur][visited] = min(dp[cur][visited], g[cur][i] + go(i, visited | (1 << i)));
+    }
+    return dp[cur][visited];
 }
-
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    int n; cin >> n;
+    cin >> n;
+    DONE = (1 << n) - 1;
     for (int i = 0; i < n; ++i) {
-        cin >> inOrder[i];
-        inOrderMap[inOrder[i]] = i;
+        for (int j = 0; j < n; ++j) {
+            int x; cin >> x;
+            if (x)
+                g[i][j] = x;
+            else
+                g[i][j] = 987654321;
+        }
     }
-    for (int i = 0; i < n; ++i) {
-        cin >> postOrder[i];
-    }
-    findPreOrder(0, n - 1, 0, n - 1);
-    cout << '\n';
+    cout << go(0, 1);
 }

cd72f772edeac255f0f6a1570aaf595dd589ae7b
hypernagox <128601833+hypernagox@users.noreply.github.com>
2024-09-09 17:07:38 +0900

    계단수

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -1,40 +1,33 @@
 #include <iostream>
-#include <algorithm>
-#include <ranges>
-#include <map>
-#include <vector>
-#include <queue>
 using namespace std;
-uint64_t dp[101][11];
+uint64_t dp[101][10][1 << 10];
 int n;
+constexpr const int MOD = 1000000000;
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     cin >> n;
-    
-    // i번째 , k로 끝나는 수
-    // dp[i][k] = dp[i-1][k-1] + dp[i-1][k+1];
-    for (int i = 0; i <= 9; ++i) {
-        dp[1][i] = 1;
-        //dp[0][i] = 1;
+    for (uint64_t i = 1; i <= 9; ++i) {
+        dp[1][i][1 << i] = 1;
     }
-    dp[1][0] = 0;
     for (int i = 2; i <= n; ++i) {
         for (int j = 0; j <= 9; ++j) {
-            if (j == 0) {
-                dp[i][j] = dp[i - 1][1] % 1000000000;
-            }
-            else if (j == 9) {
-                dp[i][j] = dp[i - 1][8] % 1000000000;
-            }
-            else {
-                dp[i][j] = ((dp[i - 1][j - 1] % 1000000000) + (dp[i - 1][j + 1] % 1000000000)) % 1000000000;
+            for (int k = 0; k < (1 << 10); ++k) {
+               if (j == 0) {
+                   dp[i][j][k | (1 << 0)] = ((dp[i][j][k | (1 << 0)] ) + (dp[i - 1][1][k] )) % MOD;
+               }
+               else if (j == 9) {
+                   dp[i][j][k | (1 << 9)] = ((dp[i][j][k | (1 << 9)] ) + (dp[i - 1][8][k] )) % MOD;
+               }
+               else {
+                   dp[i][j][k | (1 << j)] = ((dp[i][j][k | (1 << j)]) + (dp[i - 1][j - 1][k]) + (dp[i - 1][j + 1][k])) % MOD;
+               }
             }
         }
     }
     uint64_t res = 0;
     for (int i = 0; i <= 9; ++i)
-        res += (dp[n][i] % 1000000000);
-    cout << res % 1000000000;
+        res = (res % MOD + dp[n][i][(1 << 10) - 1] % MOD) % MOD;
+    cout << res;
 }

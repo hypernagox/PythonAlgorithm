b69a27ae38b45a5990882983725f2f2088b1b8a1
hypernagox <128601833+hypernagox@users.noreply.github.com>
2024-09-11 14:24:45 +0900

    선분교차

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -1,33 +1,42 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <ranges>
+#include <cmath>
 using namespace std;
-uint64_t dp[101][10][1 << 10];
-int n;
-constexpr const int MOD = 1000000000;
+using ll = long long;
+ll x, y;
+struct Point {
+	ll x, y;
+};
+vector<Point> points;
+ll ccw(const Point& a,const Point& b, const Point& c) {
+	return ((b.x - a.x) * (c.y - b.y)) - ((c.x - b.x) * (b.y - a.y));
+}
+bool check_overlap(const Point& a, const Point& b, const Point& c, const Point& d) {
+	if (min(a.x, b.x) <= max(c.x, d.x) && max(a.x, b.x) >= min(c.x, d.x) &&
+		min(a.y, b.y) <= max(c.y, d.y) && max(a.y, b.y) >= min(c.y, d.y)) {
+		return true;
+	}
+	return false;
+}
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    cin >> n;
-    for (uint64_t i = 1; i <= 9; ++i) {
-        dp[1][i][1 << i] = 1;
-    }
-    for (int i = 2; i <= n; ++i) {
-        for (int j = 0; j <= 9; ++j) {
-            for (int k = 0; k < (1 << 10); ++k) {
-               if (j == 0) {
-                   dp[i][j][k | (1 << 0)] = ((dp[i][j][k | (1 << 0)] ) + (dp[i - 1][1][k] )) % MOD;
-               }
-               else if (j == 9) {
-                   dp[i][j][k | (1 << 9)] = ((dp[i][j][k | (1 << 9)] ) + (dp[i - 1][8][k] )) % MOD;
-               }
-               else {
-                   dp[i][j][k | (1 << j)] = ((dp[i][j][k | (1 << j)]) + (dp[i - 1][j - 1][k]) + (dp[i - 1][j + 1][k])) % MOD;
-               }
-            }
-        }
-    }
-    uint64_t res = 0;
-    for (int i = 0; i <= 9; ++i)
-        res = (res % MOD + dp[n][i][(1 << 10) - 1] % MOD) % MOD;
-    cout << res;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	points.reserve(4);
+	for (int i = 0; i < 4; ++i) {
+		ll x, y; cin >> x >> y;
+		points.emplace_back(x, y);
+	}
+	const auto ccw1 = ccw(points[0], points[1], points[2]);
+	const auto ccw2 = ccw(points[0], points[1], points[3]);
+	const auto ccw3 = ccw(points[2], points[3], points[0]);
+	const auto ccw4 = ccw(points[2], points[3], points[1]);
+	if (ccw1 * ccw2 < 0 && ccw3 * ccw4 < 0) {
+		cout << 1;
+	}
+	else {
+		cout << 0;
+	}
 }

ff95d3858759b4892ee9b518d559c4f43b407ef7
hypernagox <128601833+hypernagox@users.noreply.github.com>
2025-10-05 18:46:44 +0900

    [Gold III] Title: 감시, Time: 32 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.cc
+++ 백준/Gold/15683. 감시/감시.cc
@@ -15,7 +15,7 @@ enum DIR
 	END
 };
 int n, m;
-void SpreadCCTV(vector<vector<int>>& room, pair<int, int> cur_dir, int y,int x)
+void SpreadCCTV(vector<vector<char>>& room, pair<int, int> cur_dir, int y,int x)
 {
 	const auto ny = y + cur_dir.first;
 	const auto nx = x + cur_dir.second;
@@ -34,7 +34,7 @@ struct CCTV
 	pair<int, int> Rotate(const int y, const int x) {
 		return make_pair(x, -y);
 	}
-	void ApplyRotate(const int i, vector<vector<int>>& room) {
+	void ApplyRotate(const int i, vector<vector<char>>& room) {
 		// 90도 회전 x,y -> y,-x
 		for (const auto dir : dirs)
 		{
@@ -49,7 +49,7 @@ struct CCTV
 };
 vector<CCTV> cctvs;
 int ans = 1 << 20;
-void GO(const int cur_cctv,vector<vector<int>>& room)
+void GO(const int cur_cctv,vector<vector<char>>& room)
 {
 	if (cctvs.size() == cur_cctv)
 	{
@@ -80,7 +80,7 @@ int main()
 {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> n >> m;
-	vector<vector<int>> room = vector<vector<int>>(n, vector<int>(m, 0));
+	vector<vector<char>> room = vector<vector<char>>(n, vector<char>(m, 0));
 	cctvs.reserve(8);
 	for (int i = 0; i < n; ++i)
 	{
@@ -91,6 +91,7 @@ int main()
 			if (0 != x && 6 != x)
 			{
 				CCTV c;
+				c.dirs.reserve(4);
 				switch (x)
 				{	
 				case 1:

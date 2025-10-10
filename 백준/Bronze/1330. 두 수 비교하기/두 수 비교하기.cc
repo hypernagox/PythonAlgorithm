3ae81c76783e602e4142375a4c4f81c9619c7b0f
hypernagox <128601833+hypernagox@users.noreply.github.com>
2025-10-10 23:20:59 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int a, b; cin >> a >> b;
+	if (a < b)
+	{
+		cout << "<";
+	}
+	else if (a > b)
+	{
+		cout << ">";
+	}
+	else
+	{
+		cout << "==";
+	}
+}

eff39d783c13557901e134c998c10bd247c3ca8a
hypernagox <128601833+hypernagox@users.noreply.github.com>
2025-10-12 23:42:11 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <numeric>
+#include <ranges> 
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <deque>
+#include <cmath>
+#include <span>
+#include <string_view>
+#include <sstream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int a, b; cin >> a >> b;
+	cout << a + b << '\n';
+	cout << a - b << '\n';
+	cout << a * b << '\n';
+	cout << a / b << '\n';
+	cout << a % b << '\n';
+}

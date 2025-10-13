2610bda19670a8cf2acb274c6cb1121262d62f86
hypernagox <128601833+hypernagox@users.noreply.github.com>
2025-10-13 20:28:07 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int a, b; cin >> a >> b;
+	cout << a + b;
+}

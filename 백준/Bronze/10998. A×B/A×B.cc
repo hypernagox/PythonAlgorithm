c1f73674bd61c05bde35e1c990ba8875d036fec8
hypernagox <128601833+hypernagox@users.noreply.github.com>
2025-10-15 23:03:26 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int a, b; cin >> a >> b;
+	cout << a * b;
+}

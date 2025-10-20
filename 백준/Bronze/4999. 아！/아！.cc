f70532fec37532bbdf868ebe8baf49d1ea5abb10
hypernagox <128601833+hypernagox@users.noreply.github.com>
2025-10-20 23:56:41 +0900

    [Bronze V] Title: 아!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	string str1, str2; cin >> str1 >> str2;
+	if (str1.size() >= str2.size())
+	{
+		cout << "go";
+	}
+	else
+	{
+		cout << "no";
+	}
+}

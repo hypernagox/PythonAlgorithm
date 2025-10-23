d8062a6961e8e24a172dfaf67ff7639d31adabcd
hypernagox <128601833+hypernagox@users.noreply.github.com>
2025-10-23 22:26:01 +0900

    [level 2] Title: 후보키, Time: 1.18 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/42890. 후보키/후보키.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+vector<string> comb;
+int max_idx;
+void GO(const int cur_idx = 0,string cur = "")
+{
+	for (int i = cur_idx; i < max_idx; ++i)
+	{
+		cur.push_back(i + '0');
+		comb.emplace_back(cur);
+		GO(i + 1, cur);
+		cur.pop_back();
+	}
+}
+int solution(vector<vector<string>> relation) 
+{
+	max_idx = relation.front().size();
+	GO();
+	sort(comb.begin(), comb.end(), [](auto& a, auto& b) {
+		if (a.size() == b.size())return a < b;
+		return a.size() < b.size();
+		});
+	vector<string> key_list;
+	for (const auto& c : comb)
+	{
+		set<string> s;
+		bool flag = true;
+		for (int i = 0; i < relation.size(); ++i)
+		{
+			string temp;
+			for (int j = 0; j < c.size(); ++j)
+			{
+				temp += relation[i][c[j] - '0'];
+			}
+			if (!s.emplace(temp).second)
+			{
+				flag = false;
+				break;
+			}
+		}
+		if (flag)
+		{
+			for (const auto& list : key_list)
+			{
+				string temp;
+				set_intersection(list.begin(), list.end(), c.begin(), c.end(), back_inserter(temp));
+				if (temp == list)
+				{
+					flag = false;
+					break;
+				}
+			}
+			if (flag)
+			{
+				key_list.emplace_back(c);
+			}
+		}
+	}
+	//for (const auto& i : key_list)cout << i << endl;
+	return key_list.size();
+}

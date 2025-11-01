29ba099fa5ea5b8ee5dbdad970dcf2423e344e60
hypernagox <128601833+hypernagox@users.noreply.github.com>
2025-11-01 21:53:19 +0900

    [Bronze V] Title: N 찍기, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	int n; cin >> n;
+	for (int i = 1; i <= n; ++i)cout << i << '\n';
+}

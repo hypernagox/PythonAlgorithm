1795d7d318d0f10a19bdb2aaf7807663c35891bb
hypernagox <128601833+hypernagox@users.noreply.github.com>
2025-11-02 13:12:51 +0900

    [Silver II] Title: 천상도, Time: 84 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/34042. 천상도/천상도.cc
+++ 백준/Silver/34042. 천상도/천상도.cc
@@ -7,50 +7,55 @@ int main()
 {
 	ios::sync_with_stdio(false); cin.tie(NULL);
 	int t, n; cin >> n >> t;
-	vector<int> vp, vm;
+	vector<ll> vp, vm;
 	while (t--)
 	{
-        vector<ll> pos, neg;
-        int zeros = 0;
-
-        for (int i = 0; i < n; ++i) 
-        {
-            ll x; cin >> x;
-            if (x > 0) pos.push_back(x);
-            else if (x < 0) neg.push_back(x);
-            else zeros++;
-        }
-
-        ll res = 1;
-        bool used = false;
-        for (ll x : pos) 
-        {
-            res *= x;
-            used = true;
-        }
-
-        sort(neg.begin(), neg.end());
-        for (int i = 0; i + 1 < (int)neg.size(); i += 2)
-        {
-            res *= (neg[i] * neg[i + 1]);
-            used = true;
-        }
-        if (!used) 
-        {
-           
-            if (zeros > 0) 
-            {
-                cout << 0 << '\n'; 
-            }
-            else 
-            {
-               
-                cout << neg.back() << '\n';
-            }
-        }
-        else 
-        {
-            cout << res << '\n';
-        }
+		vp.clear(); vm.clear();
+		bool flag = false;
+		bool used = false;
+		for (int i = 0; i < n; ++i)
+		{
+			int x; cin >> x;
+			if (x < 0)
+			{
+				vm.emplace_back(x);
+			}
+			else if (x > 0)
+			{
+				vp.emplace_back(x);
+				used = true;
+			}
+			else
+			{
+				flag = true;
+			}
+		}
+		sort(vm.begin(), vm.end(), greater<ll>{});
+		ll res = 1;
+		while (vm.size() >= 2)
+		{
+			const auto a1 = vm.back();
+			vm.pop_back();
+			const auto a2 = vm.back();
+			vm.pop_back();
+			res *= (a1 * a2);
+			used = true;
+		}
+		for (const auto i : vp)res *= i;
+		if (!used)
+		{
+			if (flag)
+			{
+				cout << 0 << '\n';
+			}
+			else
+			{
+				cout << vm.front() << '\n';
+			}
+		}
+		else
+		{
+			cout << res << '\n';
+		}
 	}
 }

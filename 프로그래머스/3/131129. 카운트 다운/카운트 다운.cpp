0b381d624b75a9dd2836a53623c972b884c3d7e9
hypernagox <128601833+hypernagox@users.noreply.github.com>
2025-11-02 11:05:00 +0900

    [level 3] Title: 카운트 다운, Time: 0.71 ms, Memory: 4.37 MB -BaekjoonHub

---
+++ 프로그래머스/3/131129. 카운트 다운/카운트 다운.cpp
@@ -0,0 +1,61 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+int dp[100001][2];
+vector<int> solution(int target)
+{
+    for(int i=1;i<=target;++i)
+    {
+        const auto target = i;
+        if(i <= 20 || i == 50)
+        {
+            dp[i][0] = dp[i][1] = 1;
+        }
+        else if(i%3 == 0 && i <= 60)
+        {
+            dp[i][0] = 1;
+            dp[i][1] = 0;
+        }
+        else if(i%2 == 0 && i <= 40)
+        {
+            dp[i][0] = 1;
+            dp[i][1] = 0;
+        }
+        else if(i > 50 && i <=70)
+        {
+            dp[i][0] = dp[i][1] = 2;
+        }
+        else if(i < 70)
+        {
+            if(i < 40)
+            {
+                dp[i][0] = dp[i][1]=2;
+            }
+            else
+            {
+                dp[i][0] = 2;
+                dp[i][1] = 1;
+            }
+        }
+        else
+        {
+            if(dp[target-60][0] < dp[target-50][0])
+            {
+                dp[target][0] = dp[target - 60][0] + 1;
+                dp[target][1] = dp[target - 60][1] + 0;
+            }
+            else if(dp[target-60][0] > dp[target-50][0])
+            {
+                dp[target][0] = dp[target - 50][0] + 1;
+                dp[target][1] = dp[target - 50][1] + 1;
+            }
+            else
+            {
+                dp[target][0] = dp[target - 50][0] + 1;
+                dp[target][1] = max(dp[target-60][1],dp[target-50][1] + 1);  
+            }
+        }
+    }
+    return vector<int>{dp[target][0], dp[target][1]};
+}

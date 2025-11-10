f6f66e8552aba35e9b18a5bf817c81748c8f6df5
hypernagox <128601833+hypernagox@users.noreply.github.com>
2025-11-10 16:10:46 +0900

    [Gold V] Title: 줄어드는 수, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/1174. 줄어드는 수/줄어드는 수.cc
+++ 백준/Gold/1174. 줄어드는 수/줄어드는 수.cc
@@ -2,46 +2,24 @@
 using namespace std;
 using ll = long long;
 using pi = pair<int, int>;
-vector<string> dp[11][11];
+vector<ll> res;
+void GO(const ll cur, const ll last)
+{
+	res.emplace_back(cur);
+	for (int i = 0; i < last; ++i)
+	{
+		GO(cur * 10 + i, i);
+	}
+}
 int main()
 {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	int n; cin >> n;
-	// dp[i][j] -> i자리수인데 맨앞이 j인 녀석들
-	// i는 자리수
-	for (int i = 0; i < 10; ++i)
+	for (int i = 0; i <= 9; ++i)
 	{
-		dp[1][i].emplace_back(to_string(i));
+		GO(i, i);
 	}
-	for (int i = 2; i <= 10; ++i)
-	{
-		// 맨 앞자리가 j일 때
-		for (int j = i - 1; j <= 9; ++j)
-		{
-			// i - 1 자리수인데 맨앞이 k인녀석들
-			for (int k = 0; k <= j - 1; ++k)
-			{
-				for (const auto& num : dp[i - 1][k])
-				{
-					dp[i][j].emplace_back(to_string(j) + num);
-				}
-			}
-		}
-	}
-	int cnt = 1;
-	for (const auto& i : dp)
-	{
-		for (const auto& j : i)
-		{
-			for (const auto& k : j)
-			{
-				if (cnt++ == n)
-				{
-					cout << k;
-					return 0;
-				}
-			}
-		}
-	}
-	cout << -1;
+	sort(res.begin(), res.end());
+	if (res.size() < n)cout << -1;
+	else cout << res[n - 1];
 }

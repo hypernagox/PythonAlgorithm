917681a63b6c0a49c0c293d10e22d1a5f42fbf8c
hypernagox <128601833+hypernagox@users.noreply.github.com>
2025-11-11 21:18:03 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 52.11 ms, Memory: 13 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.cpp
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.cpp
@@ -4,42 +4,51 @@
 #include <map>
 using namespace std;
 
-vector<int> solution(vector<string> gems) 
+vector<int> solution(vector<string> gems)
 {
-    const set<string> s{gems.begin(),gems.end()};
+    vector<int> answer(2,0);
+    const set<string> all_gems{gems.begin(),gems.end()};
+    map<string,int> m;
     int b = 0;
     int e = 0;
-    map<string,int> m;
-    for(const auto& g:gems)
+    // 일단 b를 무조건 시작점으로 했을 때 만족하는 구간은?
+    for(const auto& g: gems)
     {
-        ++m[g];
-        if(m.size() == s.size())break;
+        m[g]++;
+        if(m.size() == all_gems.size())break;
         ++e;
     }
-   // --e;
-    int d = e-b;
-    vector<int> answer(2);
+    int d = e - b;
     answer[0]=b+1;
     answer[1]=e+1;
+    // 일단 조건을 만족하는 상태에서 앞을 하나씩 밀어본다.
     while(e < gems.size())
     {
-        if(0 == --m[gems[b]])
+        const auto& key = gems[b];
+        ++b;
+        // 앞을 밀었더니 단어 하나가 포함이 안되게 되었다면
+        if(0 == --m[key])
         {
+            m.erase(key);
+            // 다시 조건을 만족할 때 까지 뒤를 밀어서 포함시켜본다.
             ++e;
-            m.erase(gems[b]);
-            while(e < gems.size() && m.size() != s.size())
+            while(e < gems.size() && m.size() != all_gems.size())
             {
                 ++m[gems[e]];
-                if(m.size() == s.size())break;
+                if(m.size() == all_gems.size())break;
                 ++e;
             }
         }
-        ++b;
-        if(d > e - b)
+        // 앞을 하나 민 상태에서 e를 밀어보고 조건이 맞다면
+        if(m.size() == all_gems.size())
         {
-            d=e-b;
-            answer[0]=b+1;
-            answer[1]=e+1;
+            // 원래부터 b가 가장 작을 때 부터 검사하기 때문에 길이가 같다면 먼저 검사한놈이 여기 처음온다.
+            if(d > e - b)
+            {
+                answer[0]=b+1;
+                answer[1]=e+1;
+                d = e-b;
+            }
         }
     }
     return answer;

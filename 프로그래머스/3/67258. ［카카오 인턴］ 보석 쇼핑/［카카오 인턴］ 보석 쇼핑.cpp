843c75e2045a97e85f8bd08d9cd638ebf4a0a19d
hypernagox <128601833+hypernagox@users.noreply.github.com>
2025-11-12 20:31:20 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 60.43 ms, Memory: 12.7 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.cpp
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.cpp
@@ -4,51 +4,33 @@
 #include <map>
 using namespace std;
 
-vector<int> solution(vector<string> gems)
+vector<int> solution(vector<string> gems) 
 {
     vector<int> answer(2,0);
-    const set<string> all_gems{gems.begin(),gems.end()};
+    const auto need = (int)set<string>{gems.begin(),gems.end()}.size();
     map<string,int> m;
-    int b = 0;
-    int e = 0;
-    // 일단 b를 무조건 시작점으로 했을 때 만족하는 구간은?
-    for(const auto& g: gems)
+    int have = 0;
+    int s = 0;
+    int len = 987654321;
+    for(int i=0;i<gems.size();++i)
     {
-        m[g]++;
-        if(m.size() == all_gems.size())break;
-        ++e;
-    }
-    int d = e - b;
-    answer[0]=b+1;
-    answer[1]=e+1;
-    // 일단 조건을 만족하는 상태에서 앞을 하나씩 밀어본다.
-    while(e < gems.size())
-    {
-        const auto& key = gems[b];
-        ++b;
-        // 앞을 밀었더니 단어 하나가 포함이 안되게 되었다면
-        if(0 == --m[key])
+        if(1 == ++m[gems[i]])
         {
-            m.erase(key);
-            // 다시 조건을 만족할 때 까지 뒤를 밀어서 포함시켜본다.
-            ++e;
-            while(e < gems.size() && m.size() != all_gems.size())
-            {
-                ++m[gems[e]];
-                if(m.size() == all_gems.size())break;
-                ++e;
-            }
+            ++have;
         }
-        // 앞을 하나 민 상태에서 e를 밀어보고 조건이 맞다면
-        if(m.size() == all_gems.size())
+        while(need == have)
         {
-            // 원래부터 b가 가장 작을 때 부터 검사하기 때문에 길이가 같다면 먼저 검사한놈이 여기 처음온다.
-            if(d > e - b)
+            if(len > i - s)
             {
-                answer[0]=b+1;
-                answer[1]=e+1;
-                d = e-b;
+                len = i-s;
+                answer[0]=s+1;
+                answer[1]=i+1;
             }
+            if(0 == --m[gems[s]])
+            {
+                --have;
+            }
+            ++s;
         }
     }
     return answer;

a3378ba328c45e9ae3ce140490d1afbf63ab3faa
hypernagox <128601833+hypernagox@users.noreply.github.com>
2025-11-16 20:37:44 +0900

    [Gold V] Title: 시간 관리, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/1263. 시간 관리/시간 관리.cc
+++ 백준/Gold/1263. 시간 관리/시간 관리.cc
@@ -1,87 +1,71 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+int n;
 struct Data
 {
-    ll require_time;
-    ll dead_line;
-    const auto operator<(const Data& d) const noexcept
-    {
-        if (dead_line == d.dead_line)
-        {
-            return require_time < d.require_time;
-        }
-        return dead_line < d.dead_line; // 마감시간 오름차순
-    }
+	ll dead_line;
+	ll require_time;
+	const auto operator<(const Data& d)const noexcept {
+		if (dead_line == d.dead_line)return require_time < d.require_time;
+		return dead_line < d.dead_line;
+	}
 };
-
-int n;
 vector<Data> v;
-
-bool Check(const ll start)
+bool Check(const ll mid)
 {
-    ll cur_time = start;
-    for (const auto& job : v)
-    {
-        cur_time += job.require_time;
-        if (cur_time > job.dead_line)
-        {
-            return false;
-        }
-    }
-    return true;
+	ll cur_time = mid;
+	for (const auto [d, r] : v)
+	{
+		cur_time += r;
+		if (d < cur_time)
+		{
+			return false;
+		}
+		//else
+		//{
+		//	cur_time += r;
+		//}
+	}
+	return true;
 }
-
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    cin >> n;
-    v.reserve(n);
-    for (int i = 0; i < n; ++i)
-    {
-        Data d;
-        cin >> d.require_time >> d.dead_line;
-        v.emplace_back(d);
-    }
-
-    sort(v.begin(), v.end());
-
-    // 먼저 아예 0에서 시작해도 불가능한지 체크
-    if (!Check(0))
-    {
-        cout << -1;
-        return 0;
-    }
-
-    // r: 가능한 시작 시각의 상한 + 1 (exclusive)
-    ll r = 0;
-    for (const auto& job : v)
-    {
-        r = max(r, job.dead_line);
-    }
-    ++r; // [0, r) 구간에서 이진 탐색
-
-    ll l = 0;
-    ll res = 0;
-
-    while (l < r)
-    {
-        ll mid = (l + r) / 2;
-        if (Check(mid))
-        {
-            res = mid;      // mid까지는 가능
-            l = mid + 1;    // 더 늦게 시작해볼까?
-        }
-        else
-        {
-            r = mid;        // mid는 불가능, 더 이르게
-        }
-    }
-
-    cout << res;
-    return 0;
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		Data d;
+		cin >> d.require_time >> d.dead_line;
+		//d.dead_line -= d.require_time;
+		v.emplace_back(d);
+	}
+	sort(v.begin(), v.end());
+	long long l = 0;
+	long long r = v.back().dead_line;
+	long long res = -1;
+	//for (int i = 0; i < r; ++i)
+	//{
+	//	if (!Check(i))
+	//	{
+	//		res = i - 1;
+	//		break;
+	//	}
+	//}
+	while (l < r)
+	{
+		const auto mid = (l + r) / 2;
+		if (Check(mid))
+		{
+			res = max(res, mid);
+			l = mid + 1;
+		}
+		else
+		{
+			r = mid;
+		}
+	}
+	cout << res;
 }

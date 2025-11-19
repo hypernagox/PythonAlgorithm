a80b2aa50b123323b754b79a4364d41d91cd466e
hypernagox <128601833+hypernagox@users.noreply.github.com>
2025-11-19 19:47:43 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	cout << 358 << '\n' << "jwogur";
+}

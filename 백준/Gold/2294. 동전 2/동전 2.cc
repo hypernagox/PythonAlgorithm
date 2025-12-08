242c97ee6ed0ef06d5c31f3d589e26ff2d17045f
hypernagox <128601833+hypernagox@users.noreply.github.com>
2025-12-08 17:02:52 +0900

    [Gold V] Title: 동전 2, Time: 4 ms, Memory: 2992 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -7,18 +7,15 @@ int coins[101];
 int n, k;
 int dp[200001];
 constexpr int INF = 987654321;
-int sol(int sum)
+int sol(const int sum)
 {
-    if (sum == k) return 0;   // 딱 맞으면 더 안 써도 됨
-    if (sum > k) return INF;  // 넘쳤으면 실패 경로
-
+    if (sum == k) return 0;
+    if (sum > k) return INF;
     int& ref = dp[sum];
     if (ref != -1) return ref;
-
     ref = INF;
     for (int i = 0; i < n; ++i)
     {
-        // sum + coins[i] 에서 시작했을 때의 비용 + 1
         ref = min(ref, sol(sum + coins[i]) + 1);
     }
     return ref;
@@ -32,8 +29,7 @@ int main()
 		cin >> coins[i];
 	}
     for (auto& i : dp)i = -1;
-	//sol(0);
-    int res = sol(0);
+    const int res = sol(0);
     if (res >= INF) cout << -1;
     else cout << res;
 }

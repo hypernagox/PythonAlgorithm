a395f00f84a6c7c19a73947feaebfd3cad0b6ad7
hypernagox <128601833+hypernagox@users.noreply.github.com>
2025-12-18 12:23:50 +0900

    [Gold V] Title: 동전 2, Time: 4 ms, Memory: 2248 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -1,35 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-using pi = pair<int, int>;
-using pll = pair<ll, ll>;
+constexpr const int INF = 987654321;
 int coins[101];
 int n, k;
-int dp[200001];
-constexpr int INF = 987654321;
-int sol(const int sum)
+int memo[10001];
+int GO(const int cur_money)
 {
-    if (sum == k) return 0;
-    if (sum > k) return INF;
-    int& ref = dp[sum];
-    if (ref != -1) return ref;
+    if (cur_money == 0)return 0;
+    if (cur_money < 0)return INF;
+    auto& ref = memo[cur_money];
+    if (ref)return ref;
     ref = INF;
     for (int i = 0; i < n; ++i)
     {
-        ref = min(ref, sol(sum + coins[i]) + 1);
+        ref = min(ref, GO(cur_money - coins[i]) + 1);
     }
     return ref;
 }
 int main()
 {
-	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	cin >> n >> k;
-	for (int i = 0; i < n; ++i)
-	{
-		cin >> coins[i];
-	}
-    for (auto& i : dp)i = -1;
-    const int res = sol(0);
-    if (res >= INF) cout << -1;
-    else cout << res;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> n >> k;
+    for (int i = 0; i < n; ++i)cin >> coins[i];
+    const auto ans = GO(k);
+    if (ans < INF)cout << ans;
+    else cout << -1;
 }

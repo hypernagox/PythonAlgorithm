979271924c3237d797c6283eeafb1b005b520e6b
hypernagox <128601833+hypernagox@users.noreply.github.com>
2025-12-27 17:17:52 +0900

    [Gold II] Title: 색종이 붙이기, Time: 52 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.cc
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.cc
@@ -1,78 +1,90 @@
 #include <bits/stdc++.h>
 using namespace std;
+constexpr const int INF = 987654321;
 using ll = long long;
+using ull = unsigned long long;
 using pi = pair<int, int>;
 using pll = pair<ll, ll>;
-int paper[10][10];
-int capacity[5]{ 5,5,5,5,5 };
-bool IsOk(const int y, const int x)
-{
-	if (y < 0 || x < 0 || y >= 10 || x >= 10)return false;
-	return true;
-}
-bool Check(const int y,const int x, const int size)
-{
-	for (int i = y; i < y + size; ++i)
-	{
-		for (int j = x; j < x + size; ++j)
-		{
-			if (!IsOk(i, j))return false;
-			if (0 == paper[i][j])return false;
-		}
-	}
-	return true;
-}
-void FillPaper(const int y, const int x, const int size, const int on_off)
-{
-	for (int i = y; i < y + size; ++i)
-	{
-		for (int j = x; j < x + size; ++j)
-		{
-			paper[i][j] = on_off;
-		}
-	}
-}
+using pull = pair<ull, ull>;
+int paper[11][11];
+int items[5]{ 5,5,5,5,5 };
 bool IsFinish()
 {
-	for (auto& i : paper)for (auto& j : i)if (j)return false;
-	return true;
+    for (int i = 0; i < 10; ++i)
+    {
+        for (int j = 0; j < 10; ++j)
+        {
+            if (paper[i][j])return false;
+        }
+    }
+    return true;
 }
-int res = 987654321;
-void GO(const int cnt = 0)
+bool CheckFill(const int y, const int x, const int size)
 {
-	if (IsFinish())
-	{
-		res = min(res, cnt);
-		return;
-	}
-	for (int i = 0; i < 10; ++i)
-	{
-		for (int j = 0; j < 10; ++j)
-		{
-			if (paper[i][j])
-			{
-				for (int size = 1; size <= 5; ++size)
-				{
-					// 사이즈별로 붙여봄
-					if (Check(i, j, size) && capacity[size - 1])
-					{
-						FillPaper(i, j, size, 0);
-						capacity[size - 1]--;
-						GO(cnt + 1);
-						capacity[size - 1]++;
-						FillPaper(i, j, size, 1);
-					}
-				}
-				return;
-			}
-		}
-	}
+    for (int i = y; i < y + size; ++i)
+    {
+        for (int j = x; j < x + size; ++j)
+        {
+            if (!paper[i][j])
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+void TryFill(const int y, const int x, const int size, const bool fill_flag)
+{
+    for (int i = y; i < y + size; ++i)
+    {
+        for (int j = x; j < x + size; ++j)
+        {
+            paper[i][j] = fill_flag;
+        }
+    }
+}
+int ans = INF;
+void GO(const int cur_count)
+{
+    if (IsFinish())
+    {
+        ans = min(ans, cur_count);
+        return;
+    }
+    for (int i = 0; i < 10; ++i)
+    {
+        for (int j = 0; j < 10; ++j)
+        {
+            if (paper[i][j])
+            {
+                for (int size = 1; size <= 5; ++size)
+                {
+                    if (i + size > 10 || j + size > 10)continue;
+                    if (items[size - 1] && CheckFill(i,j,size))
+                    {
+                        --items[size - 1];
+                        TryFill(i, j, size, 0);
+                        GO(cur_count + 1);
+                        TryFill(i, j, size, 1);
+                        ++items[size - 1];
+                    }
+                }
+                return;
+            }
+        }
+    }
 }
 int main()
 {
-	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	for (auto& i : paper)for (auto& j: i)cin >> j;
-	GO();
-	if (res == 987654321)cout << -1;
-	else cout << res;
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    for (int i = 0; i < 10; ++i)
+    {
+        for (int j = 0; j < 10; ++j)
+        {
+            cin >> paper[i][j];
+        }
+    }
+    GO(0);
+    if (ans == INF)cout << -1;
+    else cout << ans;
 }

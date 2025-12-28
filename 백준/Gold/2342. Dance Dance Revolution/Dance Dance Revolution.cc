5ac4e27b6be9c7e5ec6768d91a9adddd772d1e7e
hypernagox <128601833+hypernagox@users.noreply.github.com>
2025-12-28 10:33:38 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 28 ms, Memory: 19864 KB -BaekjoonHub

--- 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.cc
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.cc
@@ -6,132 +6,68 @@ using ull = unsigned long long;
 using pi = pair<int, int>;
 using pll = pair<ll, ll>;
 using pull = pair<ull, ull>;
-struct Data
-{
-    int left = 0;
-    int right = 0;
-    int cost = 0;
-    int idx = 0;
-    const auto operator<(const Data& d)const {
-        return cost > d.cost;
-    }
-};
+int n;
 int arr[100001];
-int n = 0;
-//int best[100001];
-bool visited[5][5][100001];
+int memo[5][5][100001];
+int GO(const int l, const int r, const int idx)
+{
+    if (idx == n)
+    {
+        return 0;
+    }
+    auto& ref = memo[l][r][idx];
+    if (-1 != ref)return ref;
+    const auto cur_target = arr[idx];
+    if (l == cur_target || r == cur_target)
+    {
+       return ref = GO(l, r, idx + 1) + 1;
+    }
+    else
+    {
+        int a = INF;
+        int b = INF;
+        {
+            const auto diff = abs(l - cur_target);
+            if ((diff == 1 || diff == 3) && l)
+            {
+                a = GO(cur_target, r, idx + 1) + 3;
+            }
+            else if (diff == 2 && l)
+            {
+                a = GO(cur_target, r, idx + 1) + 4;
+            }
+            else
+            {
+                a = GO(cur_target, r, idx + 1) + 2;
+            }
+        }
+        {
+            const auto diff = abs(r - cur_target);
+            if ((diff == 1 || diff == 3) && r)
+            {
+                b = GO(l, cur_target, idx + 1) + 3;
+            }
+            else if (diff == 2 && r)
+            {
+                b = GO(l, cur_target, idx + 1) + 4;
+            }
+            else
+            {
+                b = GO(l, cur_target, idx + 1) + 2;
+            }
+        }
+        return ref = min(a, b);
+    }
+}
 int main()
 {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    memset(memo, -1, sizeof(memo));
     for (int i = 0; i < 100001; ++i)
     {
         cin >> arr[i];
         if (arr[i] == 0)break;
         ++n;
     }
-    //for (auto& i : best)i = INF;
-    priority_queue<Data> pq;
-    pq.emplace(Data{});
-    set<int> res;
-    while (!pq.empty())
-    {
-        const auto [l, r, c, i] = pq.top();
-        pq.pop();
-        if (i == n)
-        {
-           // cout << c;
-            res.emplace(c);
-            continue;
-        }
-        if (visited[l][r][i])continue;
-        visited[l][r][i] = true;
-        //if (best[i] < c)
-        //{
-        //    continue;
-        //}
-        const auto cur_target = arr[i];
-        // 같다면 처리
-        if (l == cur_target)
-        {
-           // if (best[i] > c + 1)
-            {
-               // best[i] = c + 1;
-                pq.emplace(l, r, c + 1, i + 1);
-            }
-        }
-        else if (r == cur_target)
-        {
-            //if (best[i] > c + 1)
-            {
-                //best[i] = c + 1;
-                pq.emplace(l, r, c + 1, i + 1);
-            }
-           
-        }
-        else
-        {
-            // 왼발 움직여보기
-            {
-                const auto diff = abs(l - cur_target);
-                if ((diff == 1 || diff == 3) && l)
-                {
-                    //if (best[i] > c + 3)
-                    {
-                        //best[i] = c + 3;
-                        pq.emplace(cur_target, r, c + 3, i + 1);
-                    }
-                    
-                }
-                else if(diff == 2 && l)
-                {
-                    //if (best[i] > c + 4)
-                    {
-                       // best[i] = c + 4;
-                        pq.emplace(cur_target, r, c + 4, i + 1);
-                    }
-                   
-                }
-                else
-                {
-                   // if (best[i] > c + 2)
-                    {
-                       // best[i] = c + 2;
-                        pq.emplace(cur_target, r, c + 2, i + 1);
-                    }
-                }
-            }
-            // 오른발 움직여보기
-            {
-                const auto diff = abs(r - cur_target);
-                if ((diff == 1 || diff == 3 )&& r)
-                {
-                    //if (best[i] > c + 3)
-                    {
-                        //best[i] = c + 3;
-                        pq.emplace(l, cur_target, c + 3, i + 1);
-                    }
-                   
-                }
-                else if (diff == 2 && r)
-                {
-                    //if (best[i] > c + 4)
-                    {
-                        //best[i] = c + 4;
-                        pq.emplace(l, cur_target, c + 4, i + 1);
-                    }
-                    
-                }
-                else
-                {
-                    //if (best[i] > c + 2)
-                    {
-                        //best[i] = c + 2;
-                        pq.emplace(l, cur_target, c + 2, i + 1);
-                    }
-                   
-                }
-            }
-        }
-    }
-    cout << *res.begin();
+    cout << GO(0, 0, 0);
 }

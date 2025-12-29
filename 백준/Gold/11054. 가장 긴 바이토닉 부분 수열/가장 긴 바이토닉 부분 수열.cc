1006976ec93058e628dc30bc5861c45344900095
hypernagox <128601833+hypernagox@users.noreply.github.com>
2025-12-29 09:15:48 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 12 ms, Memory: 9852 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
@@ -9,56 +9,45 @@ using pull = pair<ull, ull>;
 int n;
 int arr[1001];
 int memo[1001][1001][2];
-int GO(const int idx, const int prev_idx, const bool flag)
+int GO(const int idx, const int prev_val, const bool flag)
 {
     if (idx == n)
     {
-        return 0;
+        return 1;
     }
-    int a = 0;
-    int b = 0;
-    // 고르지않았다면?
-    if (-1 == prev_idx)
-    {
-        // 처음으로 골라본다
-        a = GO(idx + 1, idx, flag) + 1;
-
-        // 또 고르지 않는다
-        b = GO(idx + 1, -1, flag);
-        return max(a, b);
-    }
-    auto& ref = memo[idx][prev_idx][flag];
+    auto& ref = memo[idx][prev_val][flag];
     if (-1 != ref)return ref;
     //ref = 0;
-   
+    int a = 0;
+    int b = 0;
     // 이제 작아져야한다면
     if (flag)
     {
         // 작아질수있다면
-        if (arr[prev_idx] > arr[idx])
+        if (prev_val > arr[idx])
         {
-            a = GO(idx + 1, idx, flag) + 1;
+            a = GO(idx + 1, arr[idx], flag) + 1;
         }
         // 그냥 선택하지 않는다.
-        b = GO(idx + 1, prev_idx, flag);
+        b = GO(idx + 1, prev_val, flag);
         return ref = max(a, b);
     }
     // 커져야한다
     else
     {
         // 고를수있다면 선택한다.
-        if (arr[prev_idx] < arr[idx])
+        if (prev_val < arr[idx])
         {
-            a = GO(idx + 1, idx, false) + 1;
+            a = GO(idx + 1, arr[idx], false) + 1;
         }
-        else if (arr[prev_idx] > arr[idx])
+        else if (prev_val > arr[idx])
         {
             // 만약 작아져볼수있다면
-            a = GO(idx + 1, idx, true) + 1;
+            a = GO(idx + 1, arr[idx], true) + 1;
         }
         // 그냥 선택하지 않는다.
-        b = GO(idx + 1, prev_idx, flag);
-        return ref = max(a,b);
+        b = GO(idx + 1, prev_val, flag);
+        return ref = max(a, b);
     }
 }
 int main()
@@ -67,5 +56,11 @@ int main()
     cin >> n;
     for (int i = 0; i < n; ++i)cin >> arr[i];
     memset(memo, -1, sizeof(memo));
-    cout << GO(0, -1, false);
+    int res = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        // 처음을 누구로?
+        res = max(res, GO(i, arr[i], false));
+    }
+    cout << res;
 }

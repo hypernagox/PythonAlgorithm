2ebac446764cf205674c99c5cd053d0d4691fad9
hypernagox <128601833+hypernagox@users.noreply.github.com>
2025-12-30 14:20:35 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 3004 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <ranges>
+using namespace std;
+int t[(501 * (501 + 1)) / 2];
+int dp[(501 * (501 + 1)) / 2];
+int n;
+int go(int idx,int d)
+{
+	if (d > n)return 0;
+	if (dp[idx])return dp[idx];
+	return dp[idx] = max(go(d + idx, d + 1), go(d + 1 + idx, d + 1)) + t[idx];
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> n;
+	int cnt = 0;
+	for (int i = 1; i <= (n * (n + 1)) / 2; ++i)cin >> t[i];
+	go(1, 1);
+	cout << dp[1];
+}

f581f91a0306bf743484c858bc96accbdc424b56
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-01-03 19:13:11 +0900

    [Gold III] Title: 색상환, Time: 4 ms, Memory: 5936 KB -BaekjoonHub

--- 백준/Gold/2482. 색상환/색상환.cc
+++ 백준/Gold/2482. 색상환/색상환.cc
@@ -8,28 +8,23 @@ using pll = pair<ll, ll>;
 using pull = pair<ull, ull>;
 constexpr const int MOD = 1000000003;
 int n, k;
-int memo[1001][1001][2];
-int GO(const int i,const int sum,const bool selected)
+int memo[1001][1001];
+int GO(const int i,const int s)
 {
-    if (i == n)return (sum == k) ? 1 : 0;
-    auto& ref = memo[i][sum][selected];
+    if (i >= n)return (s == k) ? 1 : 0;
+    auto& ref = memo[i][s];
     if (-1 != ref)return ref;
-    int res = 0;
-    // 이전에 선택하지않았으면 선택해볼수있다.
-    if (!selected)
-    {
-        res += GO(i + 1, sum + 1, true);
-        res %= MOD;
-    }
-    res += GO(i + 1, sum, false);//선택하지않고 넘어간다.
-    res %= MOD;
-    return ref = res;
+    // 고른다 -> 골랐으니 + 2번인덱스부터 시작
+    const auto a = GO(i + 2, s + 1);
+    // 고르지 않는다 -> 다음 인덱스 고려
+    const auto b = GO(i + 1, s);
+    return ref = (a + b) % MOD;
 }
 int main()
 {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> n >> k;
     memset(memo, -1, sizeof(memo));
-    // 0번을 고르지않고 쭉 + 0번고르고, 1번재끼고 마지막꺼 못고르니 n-3개에서의 경우의수
-    cout << (GO(0, 0, true) + GO(3, 1, false)) % MOD;
+    // 0번을 고르지않고  1번부터 쭉 + 0번고르고, 1번재끼고 마지막꺼 못고르니 n-3개에서의 경우의수
+    cout << (GO(1, 0) + GO(3, 1)) % MOD;
 }

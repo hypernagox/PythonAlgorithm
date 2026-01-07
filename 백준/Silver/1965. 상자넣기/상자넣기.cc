5d28dc84f5e84b819bf2796892983d2ccedc7212
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-01-07 21:27:07 +0900

    [Silver II] Title: 상자넣기, Time: 12 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+constexpr const int INF = 987654321;
+using ll = long long;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using pull = pair<ull, ull>;
+int n;
+int arr[1001];
+int memo[1001][1001];
+int GO(const int i, const int prev_idx)
+{
+    if (i == n)return 0;
+    auto& ref = memo[i][prev_idx + 1];
+    if (~ref)return ref;
+    int a = 0;
+    int b = 0;
+    if (-1 == prev_idx || arr[prev_idx] < arr[i])
+    {
+        // 선택한다.
+        a = GO(i + 1, i) + 1;
+    }
+    // 선택하지 않는다.
+    b = GO(i + 1, prev_idx);
+    return ref = max(a, b);
+}
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    for (int i = 0; i < n; ++i)cin >> arr[i];
+    memset(memo, -1, sizeof(memo));
+    cout << GO(0, -1);
+}

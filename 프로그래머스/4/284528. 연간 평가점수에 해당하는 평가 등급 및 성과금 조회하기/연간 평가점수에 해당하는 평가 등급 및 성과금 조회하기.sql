bbe65978ca139656a15580fd61f90d180ebad831
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-01-15 14:11:45 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,20 +1,27 @@
+WITH AvgScores AS (
+    SELECT
+        e.EMP_NO,
+        e.EMP_NAME,
+        e.SAL,
+        AVG(g.SCORE) AS AVG_SCORE
+    FROM HR_EMPLOYEES e
+    JOIN HR_GRADE g ON e.EMP_NO = g.EMP_NO
+    GROUP BY e.EMP_NO, e.EMP_NAME, e.SAL
+)
 SELECT
-    e.EMP_NO,
-    e.EMP_NAME,
+    EMP_NO,
+    EMP_NAME,
     CASE
-        WHEN AVG(g.SCORE) >= 96 THEN 'S'
-        WHEN AVG(g.SCORE) >= 90 THEN 'A'
-        WHEN AVG(g.SCORE) >= 80 THEN 'B'
+        WHEN AVG_SCORE >= 96 THEN 'S'
+        WHEN AVG_SCORE >= 90 THEN 'A'
+        WHEN AVG_SCORE >= 80 THEN 'B'
         ELSE 'C'
     END AS GRADE,
     CASE
-        WHEN AVG(g.SCORE) >= 96 THEN e.SAL * 0.2
-        WHEN AVG(g.SCORE) >= 90 THEN e.SAL * 0.15
-        WHEN AVG(g.SCORE) >= 80 THEN e.SAL * 0.1
+        WHEN AVG_SCORE >= 96 THEN SAL * 0.2
+        WHEN AVG_SCORE >= 90 THEN SAL * 0.15
+        WHEN AVG_SCORE >= 80 THEN SAL * 0.1
         ELSE 0
     END AS BONUS
-FROM HR_EMPLOYEES e
-JOIN HR_GRADE g ON e.EMP_NO = g.EMP_NO
-GROUP BY e.EMP_NO, e.EMP_NAME
-ORDER BY e.EMP_NO ASC
-;
+FROM AvgScores
+ORDER BY EMP_NO ASC;

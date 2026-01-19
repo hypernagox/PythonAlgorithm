116201217e0cc6bd12ecc71a9913ed877c01c25c
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-01-19 21:46:47 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+constexpr const int INF = 987654321;
+using ll = long long;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using pull = pair<ull, ull>;
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cout << "     /~\\" << endl;
+	cout << "    ( oo|" << endl;
+	cout << "    _\\=/_" << endl;
+	cout << "   /  _  \\" << endl;
+	cout << "  //|/.\\|\\\\" << endl;
+	cout << " ||  \\ /  ||" << endl;
+	cout << "============" << endl;
+	cout << "|          |" << endl;
+	cout << "|          |" << endl;
+	cout << "|          |" << endl;
+}

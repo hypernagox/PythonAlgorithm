ed37afa3a6a05c25310c529d6c979a2dd6edf65d
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-02-10 17:42:40 +0900

    [level 4] Title: [카카오 인턴] 동굴 탐험, Time: 78.85 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/4/67260. ［카카오 인턴］ 동굴 탐험/［카카오 인턴］ 동굴 탐험.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> adj[200001];
+bool visited[200001];
+unordered_map<int,int> before;
+void GO(const int cur)
+{
+    if(visited[cur])return;
+    const auto iter = before.find(cur); // 전에 가야할거있나?
+    if(before.end() != iter)
+    {
+        if(!visited[iter->second])
+        {
+            adj[iter->second].emplace_back(cur);
+            return;
+        }
+    }
+    visited[cur]=true;
+    for(int i=0;i<adj[cur].size();++i)
+    {
+        const auto next = adj[cur][i];
+        if(visited[next])continue;
+        GO(next);
+    }
+}
+bool solution(int n, vector<vector<int>> path, vector<vector<int>> order) 
+{
+    bool answer = true;
+    for(const auto& e: path)
+    {
+        adj[e[0]].emplace_back(e[1]);
+        adj[e[1]].emplace_back(e[0]);
+    }
+    before.reserve(order.size());
+    for(const auto& v: order)
+    {
+        before.emplace(v[1],v[0]);
+    }
+    GO(0);
+    for(int i=0;i<n;++i)if(!visited[i])answer=false;
+    return answer;
+}

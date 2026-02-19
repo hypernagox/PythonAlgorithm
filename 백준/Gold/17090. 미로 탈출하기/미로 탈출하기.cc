10359dbf8b2a0d317b2d6f65b23e3f52deae59c6
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-02-19 15:10:20 +0900

    [Gold III] Title: 미로 탈출하기, Time: 16 ms, Memory: 23640 KB -BaekjoonHub

--- 백준/Gold/17090. 미로 탈출하기/미로 탈출하기.cc
+++ 백준/Gold/17090. 미로 탈출하기/미로 탈출하기.cc
@@ -1,59 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = 987654321;
+constexpr const int MIN_INF = INF * -1;
 using ll = long long;
+using ull = unsigned long long;
 using pi = pair<int, int>;
 using pll = pair<ll, ll>;
-char mat[502][502];
+using pull = pair<ull, ull>;
+constexpr const int dy[]{ -1,0,1,0 };
+constexpr const int dx[]{ 0,1,0,-1 };
 int n, m;
-map<char, pi> dirs
+char board[501][501];
+int state[501][501]; // 0 미방문, 1 방문중, 2 방문 끝
+int memo[501][501];
+map<char, int> dirs
 {
-	{'U',{-1,0}},
-	{'R',{0,1}},
-	{'D',{1,0}},
-	{'L',{0,-1}},
+    {'U',0},
+    {'R',1},
+    {'D',2},
+    {'L',3},
 };
-// 0이면 아직모름
-// 1이면 불가
-// 2면 가능
-int state[501][501];
-int GO(const int y,const int x)
+bool CanGo(const int y, const int x)
 {
-	if (state[y][x])return state[y][x];
-	auto& ref = state[y][x];
-	const auto [dy, dx] = dirs[mat[y][x]];
-	const auto ny = y + dy;
-	const auto nx = x + dx;
-	if (ny < 0 || nx < 0 || ny >= n || nx >= m)
-	{
-		return ref = 2;
-	}
-	if (ref == 1)
-	{
-		// 온적이있음
-		return 1;
-	}
-	ref = 1;
-	return ref = GO(ny, nx);
+    if (y < 0 || x < 0 || y >= n || x >= m)return false;
+    return true;
+}
+int GO(const int y, const int x)
+{
+    if (!CanGo(y, x))
+    {
+        return 1;
+    }
+    if (1 == state[y][x])
+    {
+        return 0;
+    }
+    auto& ref = memo[y][x];
+    if (~ref)return ref;
+    state[y][x] = 1;
+    const auto val = board[y][x];
+    const auto dir = dirs[val];
+    const auto ny = y + dy[dir];
+    const auto nx = x + dx[dir];
+    const auto res = GO(ny, nx);
+    state[y][x] = 2;
+    return ref = res;
+}
+void Solve()noexcept
+{
+    cin >> n >> m;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            cin >> board[i][j];
+        }
+    }
+    memset(memo, -1, sizeof(memo));
+    int ans = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            ans += GO(i, j);
+        }
+    }
+    cout << ans;
 }
 int main()
 {
-	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	cin >> n >> m;
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < m; ++j)
-		{
-			cin >> mat[i][j];
-		}
-	}
-	int cnt = 0;
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < m; ++j)
-		{
-			const auto v = GO(i, j);
-			if (v == 2)++cnt;
-		}
-	}
-	cout << cnt;
+    FastIO();
+    Solve();
 }

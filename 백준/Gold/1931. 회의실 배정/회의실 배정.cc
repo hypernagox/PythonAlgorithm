06656d05dc66be7ad3c94df9e8ad29f464e446d7
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-02-21 14:27:50 +0900

    [Gold V] Title: 회의실 배정, Time: 24 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.cc
@@ -1,30 +1,44 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <ranges>
+#include <bits/stdc++.h>
 using namespace std;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = 987654321;
+constexpr const int MIN_INF = INF * -1;
+using ll = long long;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using pull = pair<ull, ull>;
+struct Data
+{
+    int start;
+    int end;
+};
+Data arr[100001];
+void Solve()noexcept
+{
+    int n; cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> arr[i].start >> arr[i].end;
+    }
+    ranges::sort(arr, arr + n, [](const auto& a, const auto& b) {
+        if (a.end == b.end)return a.start < b.start;
+        return a.end < b.end;
+        });
+    int ans = 1;
+    int cur_end = arr[0].end;
+    for (int i = 1; i < n; ++i)
+    {
+        if (cur_end <= arr[i].start)
+        {
+            ++ans;
+            cur_end = arr[i].end;
+        }
+    }
+    cout << ans;
+}
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	int n; cin >> n;
-	vector<pair<int, int>> v;
-	v.reserve(n);
-	for (int i = 0; i < n; ++i) {
-		int a, b; cin >> a >> b;
-		v.emplace_back(a, b);
-	}
-	ranges::sort(v, [](auto& a, auto& b) {
-		if (a.second == b.second) return a.first < b.first;
-		return a.second < b.second;
-		});
-	int end_time = v.front().second;
-	int res = 1;
-	for (int i = 1; i < n; ++i) {
-		if (end_time <= v[i].first) {
-			end_time = v[i].second;
-			++res;
-		}
-	}
-	cout << res;
+    FastIO();
+    Solve();
 }

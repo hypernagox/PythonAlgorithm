d9fc8b02bcf71f664efa02ba8b4924bac3ca7aa7
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-02-23 13:34:14 +0900

    [Gold IV] Title: 팰린드롬?, Time: 220 ms, Memory: 17668 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -1,40 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
-constexpr const int INF = 987654321;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<const int>(1e9);
+constexpr const int MIN_INF = -INF;
 using ll = long long;
 using ull = unsigned long long;
 using pi = pair<int, int>;
 using pll = pair<ll, ll>;
 using pull = pair<ull, ull>;
-int arr[2002];
-int memo[2002][2002];
-int GO(const int l, const int r)
+int arr[2001];
+int dp[2001][2001]; 
+int n, m;
+void Solve() noexcept
 {
-    if (l >= r)
+    cin >> n;
+    for (int i = 0; i < n; ++i) cin >> arr[i];
+
+    // len = 1
+    for (int i = 0; i < n; ++i) dp[i][i] = 1;
+
+    // len = 2
+    for (int i = 0; i + 1 < n; ++i)
     {
-        return 1;
+        if (arr[i] == arr[i + 1]) dp[i][i + 1] = 1;
     }
-    auto& ref = memo[l][r];
-    if (-1 != ref)return ref;
-    if (arr[l] == arr[r])
+
+    // len >= 3
+    for (int len = 3; len <= n; ++len)
     {
-        return ref = GO(l + 1, r - 1);
+        for (int l = 0; l + len - 1 < n; ++l)
+        {
+            int r = l + len - 1;
+            if (arr[l] == arr[r] && dp[l + 1][r - 1]) dp[l][r] = 1;
+        }
     }
-    else
+
+    cin >> m;
+    while (m--)
     {
-        return ref = 0;
+        int l, r; cin >> l >> r;
+        cout << dp[l - 1][r - 1] << '\n';
     }
 }
 int main()
 {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int n; cin >> n;
-    for (int i = 0; i < n; ++i)cin >> arr[i];
-    int m; cin >> m;
-    memset(memo, -1, sizeof(memo));
-    while (m--)
-    {
-        int a, b; cin >> a >> b;
-        cout << GO(a - 1, b - 1)<<'\n';
-    }
+    FastIO();
+    Solve();
 }

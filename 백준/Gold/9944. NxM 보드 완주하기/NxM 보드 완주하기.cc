a1f7085bd4c7538b3b98c8d9f05d16a00c16f9ea
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-02-24 09:58:47 +0900

    [Gold III] Title: NxM 보드 완주하기, Time: 440 ms, Memory: 2088 KB -BaekjoonHub

--- 백준/Gold/9944. NxM 보드 완주하기/NxM 보드 완주하기.cc
+++ 백준/Gold/9944. NxM 보드 완주하기/NxM 보드 완주하기.cc
@@ -1,84 +1,114 @@
 #include <bits/stdc++.h>
 using namespace std;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<const int>(1e9);
+constexpr const int MIN_INF = -INF;
 using ll = long long;
 using ull = unsigned long long;
 using pi = pair<int, int>;
 using pll = pair<ll, ll>;
+using pull = pair<ull, ull>;
 constexpr const int dy[]{ -1,0,1,0 };
 constexpr const int dx[]{ 0,1,0,-1 };
-int h, w;
-char board[31][31];
-bool visited[31][31];
-int ans = 987654321;
-int empty_cnt = 0;
-bool CanGo(int y, int x) 
+vector<string> board;
+int n, m;
+int visited[51][51];
+bool CanGo(const int y, const int x)
 {
-    if (y < 0 || x < 0 || y >= h || x >= w) return false;
-    if (board[y][x] == '*') return false;
-    if (visited[y][x]) return false;
+    if (y < 0 || x < 0 || y >= n || x >= m)return false;
+    if (visited[y][x])return false;
+    if (board[y][x] == '*')return false;
     return true;
 }
-void DFS(int y, int x, int cnt, int v_cnt) 
+int GetGoal()
 {
-    if (cnt >= ans) return;
-    if (v_cnt == empty_cnt)
+    int cnt = 0;
+    for (const auto& s : board)
     {
-        ans = min(ans, cnt);
+        for (const auto ch : s)
+        {
+            cnt += (ch == '.');
+        }
+    }
+    return cnt;
+}
+int ans = INF;
+int g;
+void GO(const int y, const int x, const int dir, const int num, const int d)
+{
+    auto ny = y + dy[dir];
+    auto nx = x + dx[dir];
+    if(num >= ans)return;
+    visited[y][x] = 1;
+    if (d == g)
+    {
+        ans = min(ans, num);
+        visited[y][x] = 0;
         return;
     }
-    for (int k = 0; k < 4; ++k) 
+    if (CanGo(ny, nx))
     {
-        int ny = y;
-        int nx = x;
-        vector<pair<int, int>> trace;
-       for(;;)
+       // visited[y][x] = 1;
+        GO(ny, nx, dir, num, d + 1);
+       // visited[y][x] = 0;
+    }
+    else
+    {
+        for (int k = 0; k < 4; ++k)
         {
-            const int ty = ny + dy[k];
-            const int tx = nx + dx[k];
-            if (!CanGo(ty, tx)) break;
-            ny = ty;
-            nx = tx;
-            visited[ny][nx] = true;
-            trace.push_back({ ny, nx });
+            ny = y + dy[k];
+            nx = x + dx[k];
+            if (!CanGo(ny, nx))continue;
+          //  visited[ny][nx] = 1;
+            GO(ny, nx, k, num + 1, d + 1);
+          //  visited[ny][nx] = 0;
         }
-        if (trace.empty()) continue;
-        DFS(ny, nx, cnt + 1, v_cnt + trace.size());
-        for (auto p : trace)
+    }
+    visited[y][x] = 0;
+}
+void Solve() noexcept
+{
+    int cnt = 0;
+    vector<string> anss;
+    while (cin >> n >> m)
+    {
+        //cin >> n >> m;
+        for (int i = 0; i < n; ++i)
         {
-            visited[p.first][p.second] = false;
+            string temp; cin >> temp;
+            board.emplace_back(move(temp));
         }
+        ans = INF;
+        g = GetGoal();
+        memset(visited, 0, sizeof(visited));
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                if (board[i][j] == '*')continue;
+                for (int k = 0; k < 4; ++k)
+                {
+                    //memset(visited, 0, sizeof(visited));
+                    visited[i][j] = 1;
+                    GO(i, j, k, 1, 1);
+                    visited[i][j] = 0;
+                }
+            }
+        }
+        if (ans >= INF)ans = -1;
+        if (g == 1)ans = 0;
+        stringstream sstream;
+        sstream << "Case " << ++cnt << ": " << ans << '\n';
+        anss.emplace_back(sstream.str());
+        board.clear();
+    }
+    for (const auto s : anss)
+    {
+        cout << s;
     }
 }
 int main()
 {
-    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int case_num = 1;
-    while (cin >> h >> w) 
-    {
-        ans = 987654321;
-        empty_cnt = 0;
-        memset(visited, 0, sizeof(visited));
-        for (int i = 0; i < h; ++i) 
-        {
-            for (int j = 0; j < w; ++j) 
-            {
-                cin >> board[i][j];
-                if (board[i][j] == '.') empty_cnt++;
-            }
-        }
-        for (int i = 0; i < h; ++i)
-        {
-            for (int j = 0; j < w; ++j) 
-            {
-                if (board[i][j] == '.') 
-                {
-                    visited[i][j] = true;
-                    DFS(i, j, 0, 1);
-                    visited[i][j] = false;
-                }
-            }
-        }
-        if (ans == 987654321) cout << "Case " << case_num++ << ": " << -1 << '\n';
-        else cout << "Case " << case_num++ << ": " << ans << '\n';
-    }
+    FastIO();
+    Solve();
 }

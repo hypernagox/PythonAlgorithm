69426f6303f35fa287eac620688efca285a98201
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-02-26 08:49:00 +0900

    [Gold III] Title: 연예인은 힘들어, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Gold/17270. 연예인은 힘들어/연예인은 힘들어.cc
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<int>(1e9);
+constexpr const int MIN_INF = -INF;
+using ll = long long;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using pull = pair<ull, ull>;
+int n, m, J, S;
+ll dp[101][101];
+void Solve()noexcept
+{
+    fill(&**dp, &**dp + 101 * 101, INF*2LL);
+    cin >> n >> m;
+    for (int i = 0; i < m; ++i)
+    {
+        int a, b, c; cin >> a >> b >> c;
+        dp[a][b] = min(dp[a][b], (ll)c);
+        dp[b][a] = min(dp[b][a], (ll)c);
+    }
+    cin >> J >> S;
+    for (int i = 1; i <= n; ++i)dp[i][i] = 0;
+    for (int k = 1; k <= n; ++k)
+    {
+        for (int i = 1; i <= n; ++i)
+        {
+            for (int j = 1; j <= n; ++j)
+            {
+                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]);
+            }
+        }
+    }
+    int ans = -1;
+    ll min_d = INF*2LL;
+    ll jdist = INF*2LL;
+    for (int i = 1; i <= n; ++i)
+    {
+        if (J == i || S == i)continue;
+        const auto dist = dp[J][i] + dp[S][i];
+        if (min_d > dist)
+        {
+            min_d = dist;
+        }
+    }
+    if (INF <= min_d)
+    {
+        cout << -1;
+        return;
+    }
+    for (int i = 1; i <= n; ++i)
+    {
+        if (J == i || S == i)continue;
+        if (dp[J][i] > dp[S][i])continue;
+        const auto dist = dp[J][i] + dp[S][i];
+        if (min_d == dist)
+        {
+            if (jdist > dp[J][i])
+            {
+                jdist = dp[J][i];
+                ans = i;
+            }
+        }
+    }
+    cout << ans;
+}
+int main()
+{
+    FastIO();
+    Solve();
+}

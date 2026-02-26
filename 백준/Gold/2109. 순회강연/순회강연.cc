473a9cd82262afc93657bc6bb2b69d45dbb4a076
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-02-26 13:44:03 +0900

    [Gold III] Title: 순회강연, Time: 0 ms, Memory: 2240 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.cc
+++ 백준/Gold/2109. 순회강연/순회강연.cc
@@ -1,33 +1,38 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <ranges>
-#include <numeric>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<int>(1e9);
+constexpr const int MIN_INF = -INF;
+using ll = long long;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using pull = pair<ull, ull>;
+pi arr[10001];
+void Solve()noexcept
+{
+    int n; cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> arr[i].second >> arr[i].first;
+    }
+    sort(arr, arr + n);
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for (int i = 0; i < n; ++i)
+    {
+        pq.emplace(arr[i].second);
+        if (pq.size() > arr[i].first)pq.pop();
+    }
+    int res = 0;
+    while (pq.size())
+    {
+        res += pq.top();
+        pq.pop();
+    }
+    cout << res;
+}
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int n; cin >> n;
-	int res = 0;
-	vector<pair<int, int>> v;
-	priority_queue<pair<int, int>> pq;
-	for (int i = 0; i < n; ++i) {
-		int p, d; cin >> p >> d;
-		v.emplace_back(d, -p);
-	}
-	sort(v.begin(), v.end());
-	for (const auto [d, p] : v) {
-		const auto pp = -p;
-		const auto dd = d;
-		res += pp;
-		pq.emplace(p, dd);
-		while (pq.size() > dd) {
-			res += pq.top().first; pq.pop();
-		}
-	}
-	cout << res;
+    FastIO();
+    Solve();
 }

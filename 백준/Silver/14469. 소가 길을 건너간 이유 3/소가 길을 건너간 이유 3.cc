f40b4d314551c5971f0b4609a627bc80a83ad23b
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-02-27 09:25:58 +0900

    [Silver IV] Title: 소가 길을 건너간 이유 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<int>(1e9);
+constexpr const int MIN_INF = -INF;
+using ll = long long;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using pull = pair<ull, ull>;
+pi arr[101];
+void Solve()noexcept
+{
+    int n; cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> arr[i].first >> arr[i].second;
+    }
+    sort(arr, arr + n);
+    int cur_time = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        const auto [arrive, during] = arr[i];
+        if (cur_time < arrive)
+        {
+            cur_time = arrive;
+        }
+        cur_time += during;
+    }
+    cout << cur_time;
+}
+int main()
+{
+    FastIO();
+    Solve();
+}

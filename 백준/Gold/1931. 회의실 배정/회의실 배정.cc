048384d28c3dbe76641e56b9b857834c7bbfbe33
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-02-27 14:03:24 +0900

    [Gold V] Title: 회의실 배정, Time: 24 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.cc
@@ -1,38 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
-constexpr const int INF = 987654321;
-constexpr const int MIN_INF = INF * -1;
+constexpr const int INF = static_cast<int>(1e9);
+constexpr const int MIN_INF = -INF;
 using ll = long long;
 using ull = unsigned long long;
 using pi = pair<int, int>;
 using pll = pair<ll, ll>;
 using pull = pair<ull, ull>;
-struct Data
-{
-    int start;
-    int end;
-};
-Data arr[100001];
+pi arr[100001];
 void Solve()noexcept
 {
     int n; cin >> n;
     for (int i = 0; i < n; ++i)
     {
-        cin >> arr[i].start >> arr[i].end;
+        cin >> arr[i].first >> arr[i].second;
     }
-    ranges::sort(arr, arr + n, [](const auto& a, const auto& b) {
-        if (a.end == b.end)return a.start < b.start;
-        return a.end < b.end;
+    sort(arr, arr + n, [](auto a, auto b) {
+        if (a.second == b.second)return a.first < b.first;
+        return a.second < b.second;
         });
     int ans = 1;
-    int cur_end = arr[0].end;
+    int cur_last = arr[0].second;
     for (int i = 1; i < n; ++i)
     {
-        if (cur_end <= arr[i].start)
+        if (cur_last <= arr[i].first)
         {
+            cur_last = arr[i].second;
             ++ans;
-            cur_end = arr[i].end;
         }
     }
     cout << ans;

31bd60117a3697e1b789c000be05ed7f48bd1464
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-03-01 20:17:35 +0900

    [Gold IV] Title: 자두나무, Time: 0 ms, Memory: 2268 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.cc
+++ 백준/Gold/2240. 자두나무/자두나무.cc
@@ -1,41 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<int>(1e9) + 1;
+constexpr const int MIN_INF = -INF;
 using ll = long long;
+using ull = unsigned long long;
 using pi = pair<int, int>;
 using pll = pair<ll, ll>;
+using pull = pair<ull, ull>;
+int zadu[1001];
 int t, w;
-int table[1004];
-int dp[2][1004][34];
-int aa = 0;
-int sol(const int pos, const int cur_time,const int move_count)
+int memo[1001][31][2];
+int GO(const int idx, const int nokori, const int pos)
 {
-	auto& ref = dp[pos][cur_time][move_count];
-	if (cur_time == t)
-	{
-		return 0;
-	}
-	if (ref)return ref;
-	// 움직인다
-	int a = 0;
-	int b = 0;
-	if (move_count - 1 >= 0)
-	{
-		a = sol(pos ^ 1, cur_time + 1, move_count - 1);
-	}
-	// 안움직인다
-	b = sol(pos, cur_time + 1, move_count);
-	return ref = max(a, b) + (table[cur_time] == pos);
+    if (0 > nokori)return -INF;
+    if (idx == t)
+    {
+        return 0;
+    }
+    auto& ref = memo[idx][nokori][pos];
+    if (~ref)return ref;
+    // 움직이기
+    const auto a = GO(idx + 1, nokori - 1, pos ^ 1);
+    // 안움직이기
+    const auto b = GO(idx + 1, nokori, pos);
+    return ref = max(a, b) + (pos == zadu[idx]);
+}
+void Solve()noexcept
+{
+    cin >> t >> w;
+    for (int i = 0; i < t; ++i)
+    {
+        cin >> zadu[i];
+        --zadu[i];
+    }
+    memset(memo, -1, sizeof(memo));
+    const auto a = GO(0, w, 0);
+    memset(memo, -1, sizeof(memo));
+    const auto b = GO(0, w - 1, 1);
+    cout << max(a, b);
 }
 int main()
 {
-	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	cin >> t >> w;
-	for (int i = 0; i < t; ++i)
-	{
-		cin >> table[i];
-		table[i]--;
-	}
-	cout << max(sol(0, 0, w), sol(1, 0, w - 1));
-	//int a = 10;
-	//cout << max({dp[0][t],dp[0]})
+    FastIO();
+    Solve();
 }

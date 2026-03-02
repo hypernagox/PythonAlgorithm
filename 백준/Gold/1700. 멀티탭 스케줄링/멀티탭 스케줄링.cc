d5281997b50b9aa71b3e8837583e38ebdef3a37f
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-03-02 09:17:55 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.cc
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.cc
@@ -1,49 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
-constexpr const int INF = static_cast<int>(1e9);
+constexpr const int INF = static_cast<int>(1e9) + 1;
 constexpr const int MIN_INF = -INF;
 using ll = long long;
 using ull = unsigned long long;
 using pi = pair<int, int>;
 using pll = pair<ll, ll>;
 using pull = pair<ull, ull>;
-int n, k;
-int arr[101];
-map<int, queue<int>> idxm; // 등장 인덱스
 void Solve()noexcept
 {
-    cin >> n >> k;
+    int n, k; cin >> n >> k;
+    int s[101];
+    map<int, queue<int>> m;
     for (int i = 0; i < k; ++i)
     {
-        cin >> arr[i];
-        idxm[arr[i]].emplace(i + 1);
+        int a; cin >> a;
+        m[a].emplace(i);
+        s[i] = a;
     }
+    for (auto& [num, q] : m)
+    {
+        q.emplace(INF + num);
+    }
+    map<int, int> sceduler; // 일 / 다음인덱스 
+    map<int, int> pq;
     int res = 0;
-    map<int, int> idx2item;
-    set<int> s;
     for (int i = 0; i < k; ++i)
     {
-        const auto iter = s.find(arr[i]);
-        if (s.end() != iter)
-        {
-            idxm[arr[i]].pop();
-            const auto next_idx = idxm[arr[i]].empty() ? -INF + i: -idxm[arr[i]].front();
-            idx2item.erase(-i);
-            idx2item.emplace(next_idx, arr[i]);
-            continue;
-        }
-        if (s.size() == n)
+        if (!sceduler.count(s[i]) && sceduler.size() == n)
         {
             ++res;
-            const auto [idx, num] = *idx2item.begin();
-            s.erase(num);
-            idx2item.erase(idx2item.begin());
+            const auto victim = pq.begin()->second;
+            sceduler.erase(victim);
+            pq.erase(pq.begin());
         }
-        idxm[arr[i]].pop();
-        const auto next_idx = idxm[arr[i]].empty() ? -INF + i: -idxm[arr[i]].front();
-        idx2item.emplace(next_idx, arr[i]);
-        s.emplace(arr[i]);
+        m[s[i]].pop();
+        const auto next_idx = m[s[i]].front();
+        pq.erase(-i);
+        sceduler[s[i]] = next_idx;
+        pq.emplace(-next_idx, s[i]);
     }
     cout << res;
 }

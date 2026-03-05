c9d4723e320cf04a5ec319e23c06074b2900cfe4
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-03-05 17:11:57 +0900

    [Gold III] Title: 양팔저울, Time: 0 ms, Memory: 5652 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.cc
+++ 백준/Gold/2629. 양팔저울/양팔저울.cc
@@ -1,53 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<int>(1e9) + 1;
+constexpr const int MIN_INF = -INF;
 using ll = long long;
+using ull = unsigned long long;
 using pi = pair<int, int>;
 using pll = pair<ll, ll>;
-constexpr const int INF = 987654321;
-int n;
-int k;
+using pull = pair<ull, ull>;
+int n, m;
 int chu[31];
-int orb[8];
-int memo[31][65001];
-// idx부터 고려하고 차이가 diff일때 가능한가?
-int GO(const int idx, const int diff)
+int memo[31][15001 * 2];
+int GO(const int idx, const int cur)
 {
-	if (idx == n)
-	{
-		if (0 == diff)return 1;
-		else return 0;
-	}
-	if (0 == diff)return 1;
-	auto& ref = memo[idx][diff];
-	if (-1 != ref)return ref;
-	int flag = 0;
-	// 추를 더 무거운 곳에 올려 놓는다.
-	flag |= GO(idx + 1, diff + chu[idx]);
-
-	// 추를 사용하지 않는다.
-	flag |= GO(idx + 1, diff);
-
-	// 추를 현재 더 가벼운 쪽에 올려놓는다.
-	flag |= GO(idx + 1, abs(diff - chu[idx]));
-	return ref = flag;
+    if (cur > 15000)return false;
+    if (0 == cur)return true;
+    if (idx == n)return 0 == cur;
+    auto& ref = memo[idx][cur + 15000];
+    if (~ref)return ref;
+    int res = 0;
+    res |= GO(idx + 1, cur + chu[idx]); // 물건쪽
+    res |= GO(idx + 1, cur - chu[idx]); // 물건 반대쪽
+    res |= GO(idx + 1, cur); // 이 추를 포기함
+    return ref = res;
+}
+void Solve()noexcept
+{
+    cin >> n;
+    for (int i = 0; i < n; ++i)cin >> chu[i];
+    cin >> m;
+    memset(memo, -1, sizeof(memo));
+    constexpr const char ans[]{ 'N','Y' };
+    while (m--)
+    {
+        int x; cin >> x;
+        cout << ans[GO(0, x)] << ' ';
+    }
 }
 int main()
 {
-	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	cin >> n;
-	for (int i = 0; i < n; ++i)cin >> chu[i];
-	cin >> k;
-	memset(memo, -1, sizeof(memo));
-	for (int i = 0; i < k; ++i)
-	{
-		int c; cin >> c;
-		if (1 == GO(0, c))
-		{
-			cout << 'Y' << ' ';
-		}
-		else
-		{
-			cout << 'N' << ' ';
-		}
-	}
+    FastIO();
+    Solve();
 }

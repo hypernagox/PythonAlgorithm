75de7da78fa92c6b54d11ae6e890cc7928d4a93b
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-03-07 22:06:57 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 52 ms, Memory: 9292 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<int>(1e9) + 1;
+constexpr const int MIN_INF = -INF;
+using ll = long long;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using pull = pair<ull, ull>;
+void Solve()noexcept
+{
+    int n; cin >> n;
+    int ans = 0;
+    bool flag = false;
+    set<string>& s = *new set<string>{};
+    while (n--)
+    {
+        string msg; cin >> msg;
+        if ("ENTER" == msg)
+        {
+            ans += s.size();
+            s = *new set<string>{};
+        }
+        else
+        {
+            s.emplace(move(msg));
+        }
+    }
+    cout << ans + s.size();
+}
+int main()
+{
+    FastIO();
+    Solve();
+}

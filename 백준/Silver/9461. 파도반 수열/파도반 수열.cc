c6cba4fac0114f4eff5b905684ef7589c1d6fa16
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-03-07 22:21:37 +0900

    [Silver III] Title: 파도반 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.cc
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -1,24 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<int>(1e9) + 1;
+constexpr const int MIN_INF = -INF;
 using ll = long long;
+using ull = unsigned long long;
 using pi = pair<int, int>;
 using pll = pair<ll, ll>;
-ll memo[101];
-ll GO(const int n)
+using pull = pair<ull, ull>;
+ull memo[101];
+ull GO(const int n)
 {
-	auto& ref = memo[n];
-	if (ref)return ref;
-	if (n == 1 || n == 2 || n == 3)return ref = 1; 
-	if (n == 4 || n == 5)return ref = 2;
-	return ref = GO(n - 1) + GO(n - 5);
+    if (1 == n)return 1;
+    if (2 == n)return 1;
+    if (3 == n)return 1;
+    if (4 == n)return 2;
+    if (5 == n)return 2;
+    if (memo[n])return memo[n];
+    return memo[n] = GO(n - 1) + GO(n - 5);
+}
+void Solve()noexcept
+{
+    int n; cin >> n;
+    while (n--)
+    {
+        int x; cin >> x;
+        cout << GO(x) << '\n';
+    }
 }
 int main()
 {
-	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	int n; cin >> n;
-	while (n--)
-	{
-		int x; cin >> x;
-		cout << GO(x) << '\n';
-	}
+    FastIO();
+    Solve();
 }

9e91ce9e9659a9a9e3c3d7a79f8b1892384a455a
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-03-19 17:04:38 +0900

    [Silver IV] Title: 암기왕, Time: 844 ms, Memory: 41564 KB -BaekjoonHub

---
+++ 백준/Silver/2776. 암기왕/암기왕.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<int>(1e9) + 1;
+constexpr const int MIN_INF = -INF;
+using ll = long long;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using pull = pair<ull, ull>;
+void Solve() noexcept
+{
+    int t; cin >> t;
+    unordered_set<int> us; us.reserve(1000001);
+    while (t--)
+    {
+        int n; cin >> n;
+        while (n--)
+        {
+            int x; cin >> x; us.emplace(x);
+        }
+        cin >> n;
+        while (n--)
+        {
+            int x; cin >> x;
+            cout << us.count(x) << '\n';
+        }
+        us.clear();
+    }
+}
+int main()
+{
+    FastIO();
+    Solve();
+}

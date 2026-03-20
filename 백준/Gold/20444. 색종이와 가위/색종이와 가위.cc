3b6b64eb22c705df95e365ac8b3ec64e230a0ad6
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-03-20 11:31:56 +0900

    [Gold V] Title: 색종이와 가위, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/20444. 색종이와 가위/색종이와 가위.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<int>(1e9) + 1;
+constexpr const int MIN_INF = -INF;
+using ll = long long;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using pull = pair<ull, ull>;
+void Solve() noexcept
+{
+    // (a+1) * (b+1)
+    // a + b = n
+    // b = n - a
+    ll n, k; cin >> n >> k;
+    ll l = 0;
+    ll r = n;
+    ll ans = 0;
+    while (l < r)
+    {
+        const auto mid = (l + r) / 2LL;
+        const auto val = (mid + 1) * (n - mid + 1);
+        if (val == k)
+        {
+            ans = mid;
+            cout << "YES"; return;
+        }
+        else if(val < k)
+        {
+            l = mid + 1;
+        }
+        else
+        {
+            r = mid;
+        }
+    }
+    cout << "NO";
+}
+int main()
+{
+    FastIO();
+    Solve();
+}

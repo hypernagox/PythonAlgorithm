9b92ae7bca1243120fce9af274b99024658f7a5f
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-03-21 12:27:49 +0900

    [Gold V] Title: 차트, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1239. 차트/차트.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<int>(1e9) + 1;
+constexpr const int MIN_INF = -INF;
+using ll = long long;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using pull = pair<ull, ull>;
+void Solve() noexcept
+{
+    int n; cin >> n;
+    int arr[8]{};
+    for (int i = 0; i < n; ++i)cin >> arr[i];
+    sort(arr, arr + n);
+    int ans = 0;
+    do {
+        int cnt = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            int s = 0;
+            for (int j = i + 1; j < n; ++j)
+            {
+                s += arr[j];
+                if (s >= 50)
+                {
+                    cnt += s == 50;
+                    break;
+                }
+            }
+        }
+        ans = max(ans, cnt);
+    } while (next_permutation(arr, arr + n));
+    cout << ans;
+}
+int main()
+{
+    FastIO();
+    Solve();
+}

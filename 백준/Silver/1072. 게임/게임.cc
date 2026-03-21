6e63d32fa963e2672e914985210194327e16897d
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-03-21 19:02:15 +0900

    [Silver III] Title: 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.cc
+++ 백준/Silver/1072. 게임/게임.cc
@@ -8,20 +8,20 @@ using ull = unsigned long long;
 using pi = pair<int, int>;
 using pll = pair<ll, ll>;
 using pull = pair<ull, ull>;
-ll GetPercent(const ull x, const ull y)
+ll GetPercent(const ll x, const ll y)
 {
-    return (y * 100ULL) / x;
+    return (y * 100.) / x;
 }
 void Solve() noexcept
 {
     ull x, y; cin >> x >> y;
     const auto z = GetPercent(x, y);
-    ull l = 0;
+    ull l = 1;
     ull r = 2000000000ULL;
     ll ans = -1;
     while (l < r)
     {
-        const auto mid = (l + r) / 2ULL;
+        const auto mid = (l + r) / 2LL;
         const auto val = GetPercent(x + mid, y + mid);
         if (val > z)
         {

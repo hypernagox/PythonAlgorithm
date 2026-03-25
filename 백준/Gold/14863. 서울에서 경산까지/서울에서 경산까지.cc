6ce82b72e515ee63513b87e5264f266348716f87
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-03-25 13:15:58 +0900

    [Gold IV] Title: 서울에서 경산까지, Time: 116 ms, Memory: 80928 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/14863. 서울에서 경산까지/서울에서 경산까지.cc
+++ 백준/Gold/14863. 서울에서 경산까지/서울에서 경산까지.cc
@@ -1,53 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-constexpr const int INF = 987654321;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<int>(1e9) + 1;
+constexpr const int MIN_INF = -INF;
 using ll = long long;
 using ull = unsigned long long;
 using pi = pair<int, int>;
 using pll = pair<ll, ll>;
 using pull = pair<ull, ull>;
-struct Data
-{
-    ll cost;
-    ll value;
-};
-Data walk[101];
-Data ride[101];
+pi items[2][101];
 int n, k;
 ll memo[101][100001];
-int ans = 0;
-ll GO(const int idx,const int time)
+ll GO(const int idx,const int cur_time)
 {
-    if (idx == n)
+    if (cur_time < 0)return -INF;
+    if (idx == n)return 0;
+    auto& ref = memo[idx][cur_time];
+    if (~ref)return ref;
+    ll res = -INF;
+    for (int i = 0; i < 2; ++i)
     {
-        return 0;
+        res = max(res, GO(idx + 1, cur_time - items[i][idx].first) + items[i][idx].second);
     }
-    auto& ref = memo[idx][time];
-    if (-1 != ref)return ref;
-    ref = 0;
-    ll a = 0;
-    ll b = 0;
-    // 걷기선택
-    if (time - walk[idx].cost >= 0)
+    return ref = res;
+}
+void Solve() noexcept
+{
+    cin >> n >> k;
+    for (int i = 0; i < n; ++i)
     {
-        a = GO(idx + 1, time - walk[idx].cost) + walk[idx].value;
+        for (int j = 0; j < 2; ++j)
+        {
+            cin >> items[j][i].first >> items[j][i].second;
+        }
     }
-    // 자전거선택
-    if (time - ride[idx].cost >= 0)
-    {
-        b = GO(idx + 1, time - ride[idx].cost) + ride[idx].value;
-    }
-    if (a + b == 0)return ref = -INF;
-    return ref = max(a, b);
+    memset(memo, -1, sizeof(memo));
+    cout << GO(0, k);
 }
 int main()
 {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> n >> k;
-    memset(memo, -1, sizeof(memo));
-    for (int i = 0; i < n; ++i)
-    {
-        cin >> walk[i].cost >> walk[i].value >> ride[i].cost >> ride[i].value;
-    }
-    cout << GO(0, k);
+    FastIO();
+    Solve();
 }

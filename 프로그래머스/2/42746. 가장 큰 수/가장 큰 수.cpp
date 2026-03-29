1448f0be33dddea243ca68716469d737004fcf43
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-03-29 12:40:02 +0900

    [level 2] Title: 가장 큰 수, Time: 38.56 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -3,39 +3,23 @@ using namespace std;
 using ll = long long;
 using pi = pair<int, int>;
 using pll = pair<ll, ll>;
-
-struct Data
-{
-    int counts[10]{}; // arr[i]는 이 문자열에서 i가 몇번 등장했는가 ? , 인덱스는 몇번인가?
-    int real_val;
-    string str;
-    const auto operator<(const Data& d)const noexcept {
-        auto a = str + d.str;
-        auto b = d.str + str;
-        return stoi(a) > stoi(b);
-    }
-    Data(const int i)
-        :real_val{ i }, str{ to_string(i) }
-    {
-        for (int i = 0; i < str.size(); ++i)
-        {
-            ++counts[str[i] - '0'];
-        }
-    }
-};
 string solution(vector<int> numbers) 
 {
-    string answer = "";
-    vector<Data> d; d.reserve(numbers.size());
-    for (const auto i : numbers)
+    vector<string> v;
+    for(const auto n : numbers)
     {
-        d.emplace_back(i);
+        v.emplace_back(to_string(n));
     }
-    sort(d.begin(), d.end());
-    for (const auto& data : d)
-    {
-        answer += data.str;
-    }
-    if (answer[0] == '0')return "0";
+    sort(v.begin(),v.end(),[](const auto& a,const auto& b){
+        for(int i=0;i<min(a.size(),b.size());++i)
+        {
+            if(a[i]==b[i])continue;
+            return a[i] > b[i];
+        }
+        return a + b > b + a;
+    });
+    string answer{};
+    for(const auto& s : v)answer += s;
+    if(answer[0]=='0')return "0";
     return answer;
 }

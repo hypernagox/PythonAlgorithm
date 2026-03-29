5e7b3de6fe54823c6014a7e3003e0ba46cd142d1
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-03-29 18:04:51 +0900

    [Silver I] Title: 맥주 축제, Time: 680 ms, Memory: 9368 KB -BaekjoonHub

---
+++ 백준/Silver/17503. 맥주 축제/맥주 축제.cc
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<int>(1e9) + 1;
+constexpr const int MIN_INF = -INF;
+using ll = long long;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using pull = pair<ull, ull>;
+ll n, m, k;
+pll arr[200001];
+bool Check(const ll mid)
+{
+    priority_queue<ll> pq;
+    for (int i = 0; i < k; ++i)
+    {
+        const auto [level, prio] = arr[i];
+        if (level > mid)break;
+        pq.emplace(prio);
+    }
+    if (pq.size() < n)return false;
+    ll s = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        s += pq.top();
+        pq.pop();
+    }
+    return s >= m;
+}
+void Solve() noexcept
+{
+    cin >> n >> m >> k;
+    for (int i = 0; i < k; ++i)
+    {
+        cin >> arr[i].second >> arr[i].first;
+    }
+    sort(arr, arr + k);
+    ll low = 0;
+    ll high = INT32_MAX + 1LL;
+    ll ans = -1;
+    while (low < high)
+    {
+        const auto mid = low + (high - low) / 2LL;
+        if (Check(mid))
+        {
+            ans = mid;
+            high = mid;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    cout << ans;
+}
+int main()
+{
+    FastIO();
+    Solve();
+}

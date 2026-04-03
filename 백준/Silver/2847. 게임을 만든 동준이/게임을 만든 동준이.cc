48c57c980d13b1d6f98293cd4c716c59dcdf07c1
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-04-03 13:30:11 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.cc
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.cc
@@ -8,28 +8,23 @@ using ull = unsigned long long;
 using pi = pair<int, int>;
 using pll = pair<ll, ll>;
 using pull = pair<ull, ull>;
-void Solve()noexcept
+int arr[101];
+int n;
+void Solve() noexcept
 {
     int n; cin >> n;
-    int arr[100];
     for (int i = 0; i < n; ++i)cin >> arr[i];
-    int delta = 0;
-    int ans = 0;
-    for (int i = n - 1; i >= 1; --i)
+    int s = 0;
+    for (int i = n - 2; i >= 0; --i)
     {
-        const auto high = arr[i] - delta;
-        const auto low = arr[i - 1];
-        if (high <= low)
+        if (arr[i + 1] <= arr[i])
         {
-            delta = (low - high) + 1;
-            ans += delta;
-        }
-        else
-        {
-            delta = 0;
+            const auto delta = arr[i] - arr[i + 1];
+            s += delta + 1;
+            arr[i] -= (delta + 1);
         }
     }
-    cout << ans;
+    cout << s;
 }
 int main()
 {

cf122cc86d13a67cfd21361027be6fd247068dfd
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-04-04 18:47:46 +0900

    [Gold II] Title: 택배, Time: 0 ms, Memory: 2056 KB, Score: 52 point -BaekjoonHub

---
+++ 백준/Gold/8980. 택배/택배.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<int>(1e9) + 1;
+constexpr const int MIN_INF = -INF;
+using ll = long long;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using pull = pair<ull, ull>;
+struct Data
+{
+    int from;
+    int to;
+    int amount;
+    const auto operator<(const Data& d)const {
+        if (to == d.to)return from < d.from;
+        return to < d.to;
+    }
+};
+Data items[2001];
+int capacities[2001];
+void Solve() noexcept
+{
+    int n, c, m; cin >> n >> c >> m;
+    fill(capacities, capacities + n, c);
+    for (int i = 0; i < m; ++i)
+    {
+        int a, b, c; cin >> a >> b >> c;
+        items[i] = Data{ a - 1,b - 1,c };
+    }
+    sort(items, items + n);
+    int ans = 0;
+    for (const auto [from, to, amount] : items)
+    {
+        const auto remain_cap = *min_element(capacities + from, capacities + to);
+        const auto delta = min(amount, remain_cap);
+        ans += delta;
+        transform(capacities + from, capacities + to, capacities + from, [delta](const int num) {
+            return num - delta;
+            });
+    }
+    cout << ans;
+}
+int main()
+{
+    FastIO();
+    Solve();
+}

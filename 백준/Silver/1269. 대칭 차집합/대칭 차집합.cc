b740e39f0f7d492b7785efcf99c4351343ba0c0e
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-04-04 21:47:45 +0900

    [Silver IV] Title: 대칭 차집합, Time: 76 ms, Memory: 3592 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -8,24 +8,31 @@ using ull = unsigned long long;
 using pi = pair<int, int>;
 using pll = pair<ll, ll>;
 using pull = pair<ull, ull>;
-int A[200000];
-int B[200000];
 void Solve() noexcept
 {
     int n, m; cin >> n >> m;
-    for (int i = 0; i < n; ++i)cin >> A[i];
-    for (int i = 0; i < m; ++i)cin >> B[i];
-    ranges::sort(A, A + n);
-    ranges::sort(B, B + m);
+    vector<int> A, B; A.reserve(n); B.reserve(m);
+    for (int i = 0; i < n; ++i) 
+    {
+        int x; cin >> x;
+        A.emplace_back(x);
+    }
+    for (int i = 0; i < m; ++i)
+    {
+        int x; cin >> x;
+        B.emplace_back(x);
+    }
+    ranges::sort(A);
+    ranges::sort(B);
     int a_size = n;
     int b_size = m;
     for (int i = 0; i < n; ++i)
     {
-        b_size -= ranges::binary_search(B, B + m, A[i]);
+        b_size -= ranges::binary_search(B, A[i]);
     }
     for (int i = 0; i < m; ++i)
     {
-        a_size -= ranges::binary_search(A, A + n, B[i]);
+        a_size -= ranges::binary_search(A, B[i]);
     }
     cout << a_size + b_size;
 }

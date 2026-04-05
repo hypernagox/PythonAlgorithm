631c658b863b47c18e8eb07313ca5ebcc0a823eb
hypernagox <hypernagox@tukorea.ac.kr>
2026-04-06 08:38:01 +0900

    그리디 모음집 업데이트

--- AA/backjoon.cpp
+++ AA/backjoon.cpp
@@ -1,9 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<int>(1e9) + 1;
+constexpr const int MIN_INF = -INF;
 using ll = long long;
+using ull = unsigned long long;
 using pi = pair<int, int>;
 using pll = pair<ll, ll>;
+using pull = pair<ull, ull>;
+void Solve() noexcept
+{
+
+}
 int main()
 {
-	ios::sync_with_stdio(false); cin.tie(NULL);
+    FastIO();
+    Solve();
 }

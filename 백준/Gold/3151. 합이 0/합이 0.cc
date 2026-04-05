fd4551ff47e822c7730c288aaaabbb9008159ca1
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-04-06 07:45:25 +0900

    [Gold IV] Title: 합이 0, Time: 2056 ms, Memory: 2184 KB -BaekjoonHub

---
+++ 백준/Gold/3151. 합이 0/합이 0.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<int>(1e9) + 1;
+constexpr const int MIN_INF = -INF;
+using ll = long long;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using pull = pair<ull, ull>;
+void Solve() noexcept
+{
+    int n; cin >> n;
+    vector<int> v; v.reserve(n);
+    for (int i = 0; i < n; ++i)
+    {
+        int x; cin >> x;
+        v.emplace_back(x);
+    }
+    sort(v.begin(), v.end());
+    ll ans = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        const auto a = v[i];
+        for (int j = i + 1; j < n; ++j)
+        {
+            const auto b = v[j];
+            const auto target = -(a + b);
+            const auto iter = lower_bound(v.begin() + j + 1, v.end(), target);
+            const auto iter2 = upper_bound(v.begin() + j + 1, v.end(), target);
+            if (v.end() != iter && *iter == target)
+            {
+                ans += (iter2 - iter);
+            }
+        }
+    }
+    cout << ans;
+}
+int main()
+{
+    FastIO();
+    Solve();
+}

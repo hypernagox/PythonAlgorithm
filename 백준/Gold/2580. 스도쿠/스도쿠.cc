97abfda186cd9f3405ae32bf7e261cf662dbbbb8
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-04-06 11:10:48 +0900

    [Gold IV] Title: 스도쿠, Time: 344 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.cc
+++ 백준/Gold/2580. 스도쿠/스도쿠.cc
@@ -1,55 +1,81 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-int sudoku[9][9];
-pair<int, int> nokori[81];
-int cnt;
-bool Check(int r,int c)
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<int>(1e9) + 1;
+constexpr const int MIN_INF = -INF;
+using ll = long long;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using pull = pair<ull, ull>;
+string board[9];
+vector<pi> nokori;
+bool Check(const int y, const int x)
 {
-	for (int i = 0; i < 9; ++i) {
-		if (sudoku[r][c] == sudoku[r][i] && c != i)return false;
-		if (sudoku[r][c] == sudoku[i][c] && r != i)return false;
-	}
-	const int ry = (r / 3) * 3;
-	const int rx = (c / 3) * 3;
-	for (int y = ry; y < ry + 3; ++y) {
-		for (int x = rx; x < rx + 3; ++x) {
-			if (r == y && x == c)continue;
-			if (sudoku[r][c] == sudoku[y][x])return false;
-		}
-	}
-	return true;
+    for (int i = 0; i < 9; ++i)
+    {
+        if (board[y][x] == board[i][x] && i != y)return false;
+        if (board[y][x] == board[y][i] && i != x)return false;
+    }
+    const auto yy = (y / 3) * 3;
+    const auto xx = (x / 3) * 3;
+    for (int i = yy; i < yy + 3; ++i)
+    {
+        for (int j = xx; j < xx + 3; ++j)
+        {
+            if (y == i && x == j)continue;
+            if (board[i][j] == board[y][x])return false;
+        }
+    }
+    return true;
 }
-void go(int n = 0)
+int n;
+void GO(const int cnt = 0)
 {
-	if (n == cnt) {
-		for (int i = 0; i < 9; ++i) {
-			for (int j = 0; j < 9; ++j) {
-				cout << sudoku[i][j] << ' ';
-			}
-			cout << '\n';
-		}
-		exit(0);
-	}
-	const auto [y, x] = nokori[n];
-	for (int j = 1; j <= 9; ++j) {
-		sudoku[y][x] = j;
-		if (Check(y, x))
-			go(n + 1);
-	}
-	sudoku[y][x] = 0;
+    if (cnt == n)
+    {
+        for (int i = 0; i < 9; ++i)
+        {
+            for (int j = 0; j < 9; ++j)
+            {
+                cout << board[i][j] << ' ';
+            }
+            cout << '\n';
+        }
+        exit(0);
+        return;
+    }
+    const auto [i, j] = nokori[cnt];
+    for (int k = 1; k <= 9; ++k)
+    {
+        const char target = k + '0';
+        board[i][j] = target;
+        if (Check(i, j))
+        {
+            GO(cnt + 1);
+        }
+        board[i][j] = 0 + '0';
+    }
+}
+void Solve() noexcept
+{
+    for (int i = 0; i < 9; ++i)
+    {
+        for (int j = 0; j < 9; ++j)
+        {
+            char c; cin >> c;
+            if (c == '0')
+            {
+                ++n;
+                nokori.emplace_back(i, j);
+            }
+            board[i] += c;
+        }
+    }
+    GO();
 }
 int main()
 {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	for (int i = 0; i < 9; ++i) {
-		for (int j = 0; j < 9; ++j) {
-			int c; cin >> c;
-			if (c==0)
-				nokori[cnt++] = { i,j };
-			else
-				sudoku[i][j] = c;
-		}
-	}
-	go();
+    FastIO();
+    Solve();
 }

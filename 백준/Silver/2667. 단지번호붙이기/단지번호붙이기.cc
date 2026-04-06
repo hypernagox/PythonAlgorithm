1968af31b98c05b86529d843f9a70f1f31eb593a
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-04-06 13:50:29 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<int>(1e9) + 1;
+constexpr const int MIN_INF = -INF;
+using ll = long long;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using pull = pair<ull, ull>;
+int n;
+char board[26][26];
+constexpr const int dy[]{ -1,0,1,0 };
+constexpr const int dx[]{ 0,1,0,-1 };
+bool visited[26][26];
+int GO(const int y, const int x)
+{
+    visited[y][x] = 1;
+    int res = 1;
+    for (int i = 0; i < 4; ++i)
+    {
+        const auto ny = y + dy[i];
+        const auto nx = x + dx[i];
+        if (ny < 0 || nx < 0 || ny >= n || nx >= n)continue;
+        if (visited[ny][nx])continue;
+        if (board[ny][nx] == '0')continue;
+        res += GO(ny, nx);
+    }
+    return res;
+}
+void Solve() noexcept
+{
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            cin >> board[i][j];
+        }
+    }
+    vector<int> res;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            if (board[i][j] == '0')continue;
+            if (visited[i][j])continue;
+            res.emplace_back(GO(i, j));
+        }
+    }
+    sort(res.begin(), res.end());
+    cout << res.size() << '\n';
+    for (const auto i : res)cout << i << '\n';
+}
+int main()
+{
+    FastIO();
+    Solve();
+}

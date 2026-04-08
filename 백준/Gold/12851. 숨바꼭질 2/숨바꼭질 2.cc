03b260210c665e0c254c37c0bdfaab6d20dd4e10
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-04-08 11:36:51 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 24 ms, Memory: 6404 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -9,15 +9,6 @@ using pi = pair<int, int>;
 using pll = pair<ll, ll>;
 using pull = pair<ull, ull>;
 int visited[100001];
-bool Check(const int next, const int cost)
-{
-    if (!(next < 0 || next >100000) && visited[next] >= cost + 1)
-    {
-        visited[next] = cost + 1;
-        return true;
-    }
-    return false;
-}
 void Solve() noexcept
 {
     int src, dest; cin >> src >> dest;
@@ -30,27 +21,17 @@ void Solve() noexcept
     {
         const auto [cur, cost] = q.front();
         q.pop();
+        if (cur < 0 || cur >100000)continue;
         if (visited[cur] < cost)continue;
+        visited[cur] = cost;
         if (cur == dest)
         {
             ++ans;
             continue;
         }
-        auto next = cur + 1;
-        if (Check(next,cost))
-        {
-            q.emplace(next, cost + 1);
-        }
-        next = cur - 1;
-        if (Check(next, cost))
-        {
-            q.emplace(next, cost + 1);
-        }
-        next = cur * 2;
-        if (Check(next, cost))
-        {
-            q.emplace(next, cost + 1);
-        }
+        q.emplace(cur + 1, cost + 1);
+        q.emplace(cur - 1, cost + 1);
+        q.emplace(cur * 2, cost + 1);
     }
     cout << visited[dest] << '\n' << ans;
 }

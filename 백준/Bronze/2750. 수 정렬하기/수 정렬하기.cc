0ced741299a23713e9cfe676291012649c3413ff
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-04-09 10:45:01 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -33,9 +33,52 @@ void QuickSort(vector<int>& arr, const int left, const int right)
         QuickSort(arr, pivot + 1, right);
     }
 }
+void Partition2(vector<int>& arr, const int left, const int right)
+{
+    const auto mid = (left + right) / 2;
+    const auto left_size = mid - left + 1;
+    const auto right_size = right - mid;
+    vector<int> L(left_size);
+    vector<int> R(right_size);
+    for (int i = 0; i < left_size; ++i)
+    {
+        L[i] = arr[left + i];
+    }
+    for (int i = 0; i < right_size; ++i)
+    {
+        R[i] = arr[mid + 1 + i];
+    }
+    int l = 0;
+    int r = 0;
+    int k = left;
+    while (l < left_size && r < right_size)
+    {
+        if (L[l] <= R[r])
+        {
+            arr[k++] = L[l++];
+        }
+        else
+        {
+            arr[k++] = R[r++];
+        }
+    }
+    while (l < left_size){ arr[k++] = L[l++]; }
+    while (r < right_size) { arr[k++] = R[r++]; }
+}
+void MergeSort(vector<int>& arr, const int left, const int right)
+{
+    if (left < right)
+    {
+        const auto mid = (left + right) / 2;
+        MergeSort(arr, left, mid);
+        MergeSort(arr, mid + 1, right);
+        Partition2(arr, left, right);
+    }
+}
 void Sort(vector<int>& arr, const int left, const int right)
 {
-    QuickSort(arr, left, right);
+    //QuickSort(arr, left, right);
+    MergeSort(arr, left, right);
 }
 void Solve() noexcept
 {

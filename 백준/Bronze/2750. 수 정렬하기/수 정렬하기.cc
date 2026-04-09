6813da50592511e39f2349d40bb801c247baac00
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-04-09 11:14:22 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -75,10 +75,116 @@ void MergeSort(vector<int>& arr, const int left, const int right)
         Partition2(arr, left, right);
     }
 }
+class Heap
+{
+public:
+    void Add(const int val)
+    {
+        arr[size] = val;
+        HeapifyUp(size);
+        ++size;
+    }
+    int Pop()
+    {
+        const auto target = arr[0];
+        arr[0] = arr[size - 1];
+        --size;
+        HeapifyDown(0);
+        return target;
+    }
+private:
+    void HeapifyUp(const int idx)
+    {
+        int i = idx;
+        while (i > 0 && arr[i] > arr[(i - 1) / 2])
+        {
+            swap(arr[i], arr[(i - 1) / 2]);
+            i = (i - 1) / 2;
+        }
+    }
+    void HeapifyDown(const int idx)
+    {
+        int i = idx;
+        while (i < size)
+        {
+            int largest = i;
+            int left = i * 2 + 1;
+            int right = i * 2 + 2;
+            if (left < size && arr[largest] < arr[left])largest = left;
+            if (right < size && arr[largest] < arr[right])largest = right;
+            if (largest == i)break;
+            swap(arr[largest], arr[i]);
+            i = largest;
+        }
+    }
+private:
+    static constexpr const int MAX_SIZE = 1001;
+    int size = 0;
+    int arr[MAX_SIZE];
+};
+void HeapifyDown(vector<int>& arr, const int heap_size, int index)
+{
+    while (true)
+    {
+        int largest = index;
+        const int left = index * 2 + 1;
+        const int right = index * 2 + 2;
+
+        if (left < heap_size && arr[left] > arr[largest])
+        {
+            largest = left;
+        }
+        if (right < heap_size && arr[right] > arr[largest])
+        {
+            largest = right;
+        }
+
+        if (largest == index)
+        {
+            break;
+        }
+
+        swap(arr[index], arr[largest]);
+        index = largest;
+    }
+}
+
+void BuildMaxHeap(vector<int>& arr)
+{
+    const int n = static_cast<int>(arr.size());
+    for (int i = n / 2 - 1; i >= 0; --i)
+    {
+        HeapifyDown(arr, n, i);
+    }
+}
+
+void HeapSort(vector<int>& arr)
+{
+    const int n = static_cast<int>(arr.size());
+
+    BuildMaxHeap(arr);
+
+    for (int i = n - 1; i > 0; --i)
+    {
+        swap(arr[0], arr[i]);
+        HeapifyDown(arr, i, 0);
+    }
+}
 void Sort(vector<int>& arr, const int left, const int right)
 {
     //QuickSort(arr, left, right);
-    MergeSort(arr, left, right);
+   // MergeSort(arr, left, right);
+
+    //Heap h;
+    //for (int i = 0; i < arr.size(); ++i)
+    //{
+    //    h.Add(arr[i]);
+    //}
+    //for (int i = 0; i < arr.size(); ++i)
+    //{
+    //    cout << h.Pop() << '\n';
+    //}
+    HeapSort(arr);
 }
 void Solve() noexcept
 {

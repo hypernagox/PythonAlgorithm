d52b34e232893e7af33e635f5e28dea59f000a59
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-04-09 21:56:13 +0900

    [Gold II] Title: 장난감 조립, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2637. 장난감 조립/장난감 조립.cc
+++ 백준/Gold/2637. 장난감 조립/장난감 조립.cc
@@ -1,53 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
-constexpr const int INF = 987654321;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<int>(1e9) + 1;
+constexpr const int MIN_INF = -INF;
 using ll = long long;
 using ull = unsigned long long;
 using pi = pair<int, int>;
 using pll = pair<ll, ll>;
 using pull = pair<ull, ull>;
-int inDegree[101];
 vector<pi> adj[101];
-int res[101][101];
-int main()
+int n, m;
+int inDegree[101];
+int memo[101];
+int GO(const int cur)
 {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int n, m; cin >> n >> m;
-    vector<int> ans;
-    queue<int> q;
-    while (m--)
+    if (adj[cur].empty())return 1;
+    auto& ref = memo[cur];
+    if (ref)return ref;
+    int res = 0;
+    for (const auto [c, next] : adj[cur])
+    {
+        res += GO(next) * c;
+    }
+    return ref = res;
+}
+
+void Solve() noexcept
+{
+    cin >> n >> m;
+    for (int i = 0; i < m; ++i)
     {
         int a, b, c; cin >> a >> b >> c;
         adj[b].emplace_back(c, a);
         ++inDegree[a];
     }
-    for (int i = 0; i < n; ++i)
+    vector<int> base;
+    for (int i = 1; i <= n; ++i)
     {
-        if (0 == inDegree[i + 1])
+        if (inDegree[i] == 0)
         {
-            q.emplace(i + 1);
-            ans.emplace_back(i + 1);
-            res[i + 1][i + 1] = 1;
+            base.emplace_back(i);
         }
     }
-    while (!q.empty())
+    for (const auto i : base)
     {
-        const auto cur = q.front();
-        q.pop();
-        for (const auto [cost, next] : adj[cur])
-        {
-            for (int b = 1; b <= n; ++b)
-            {
-                if (res[cur][b] != 0)
-                {
-                    res[next][b] += res[cur][b] * (ll)cost;
-                }
-            }
-            if (0 == --inDegree[next])
-            {
-                q.emplace(next);
-            }
-        }
+        cout << i << ' ' << GO(i) << '\n';
     }
-    for (const auto i : ans)cout << i << ' ' << res[n][i] << '\n';
+}
+int main()
+{
+    FastIO();
+    Solve();
 }

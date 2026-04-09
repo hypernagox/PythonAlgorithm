01a02d0966e53e2698c0872405447e26ae9b0247
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-04-09 11:34:48 +0900

    [Gold IV] Title: 알파벳, Time: 464 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,47 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
-constexpr const int INF = 987654321;
+constexpr const int INF = static_cast<int>(1e9) + 1;
+constexpr const int MIN_INF = -INF;
 using ll = long long;
 using ull = unsigned long long;
 using pi = pair<int, int>;
 using pll = pair<ll, ll>;
 using pull = pair<ull, ull>;
+int r, c;
+char board[21][21];
 constexpr const int dy[]{ -1,0,1,0 };
 constexpr const int dx[]{ 0,1,0,-1 };
-constexpr const int DIRS = sizeof(dy) / sizeof(dy[0]);
-char board[21][21];
-int r, c;
-unordered_map<ll, int> memo;
-ll MakeKey(const int y, const int x, const int state)
+bool visited[26];
+int GO(const int y, const int x)
 {
-    return (ll)(state) << 32 | y << 8 | x;
-}
-int GO(const int y, const int x, const int state)
-{
-    const auto key = MakeKey(y, x, state);
-    const auto iter = memo.find(key);
-    if (memo.end() != iter)return iter->second;
+    const auto val = board[y][x] - 'A';
+    visited[val] = 1;
     int res = 1;
-    for (int i = 0; i < DIRS; ++i)
+    for (int i = 0; i < 4; ++i)
     {
         const auto ny = y + dy[i];
         const auto nx = x + dx[i];
-        if (ny < 0 || nx < 0 || ny >= r || nx >= c)
-        {
-            continue;
-        }
-        const auto next_alpha= board[ny][nx] - 'A';
-        if (state & (1 << next_alpha))
-        {
-            continue;
-        }
-        res = max(res, GO(ny, nx, state | (1 << next_alpha)) + 1);
+        if (ny < 0 || nx < 0 || ny >= r || nx >= c)continue;
+        const auto v = board[ny][nx] - 'A';
+        if (visited[v])continue;
+        res = max(res, GO(ny, nx) + 1);
     }
-    memo.emplace(key, res);
+    visited[val] = 0;
     return res;
 }
-void Solve()noexcept
+void Solve() noexcept
 {
     cin >> r >> c;
     for (int i = 0; i < r; ++i)
@@ -51,8 +40,7 @@ void Solve()noexcept
             cin >> board[i][j];
         }
     }
-    memo.reserve(r * c * 26);
-    cout << GO(0, 0, 1 << (board[0][0] - 'A'));
+    cout << GO(0, 0);
 }
 int main()
 {

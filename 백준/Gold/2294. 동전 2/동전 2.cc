23733ed82cc92a494c710b7e6a536982daf01a74
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-04-12 16:11:49 +0900

    [Gold V] Title: 동전 2, Time: 12 ms, Memory: 6320 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -1,29 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-constexpr const int INF = 987654321;
-int coins[101];
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<int>(1e9) + 1;
+constexpr const int MIN_INF = -INF;
+using ll = long long;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using pull = pair<ull, ull>;
 int n, k;
-int memo[10001];
-int GO(const int cur_money)
+int arr[101];
+int memo[101][10001];
+int GO(const int idx, const int cur_money)
 {
-    if (cur_money == 0)return 0;
     if (cur_money < 0)return INF;
-    auto& ref = memo[cur_money];
-    if (ref)return ref;
-    ref = INF;
-    for (int i = 0; i < n; ++i)
-    {
-        ref = min(ref, GO(cur_money - coins[i]) + 1);
-    }
-    return ref;
+    if (cur_money == 0)return 0;
+    if (idx == n)return cur_money == 0 ? 0 : INF;
+    auto& ref = memo[idx][cur_money];
+    if (~ref)return ref;
+    // 사용
+    const auto a = GO(idx, cur_money - arr[idx]) + 1;
+    // 안사용
+    const auto b = GO(idx + 1, cur_money);
+    return ref = min(a, b);
+}
+void Solve() noexcept
+{
+    cin >> n >> k;
+    for (int i = 0; i < n; ++i)cin >> arr[i];
+    memset(memo, -1, sizeof(memo));
+    const auto ans = GO(0, k);
+    if (ans >= INF)cout << -1;
+    else cout << ans;
 }
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    cin >> n >> k;
-    for (int i = 0; i < n; ++i)cin >> coins[i];
-    const auto ans = GO(k);
-    if (ans < INF)cout << ans;
-    else cout << -1;
+    FastIO();
+    Solve();
 }

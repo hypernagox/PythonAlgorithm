cd5e12725e8c63433ed4dcc4fc185a2167005e72
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-04-12 16:11:34 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 4 ms, Memory: 5948 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,32 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<int>(1e9) + 1;
+constexpr const int MIN_INF = -INF;
 using ll = long long;
+using ull = unsigned long long;
 using pi = pair<int, int>;
-int dp[1001];
+using pll = pair<ll, ll>;
+using pull = pair<ull, ull>;
 int arr[1001];
+int n;
+int memo[1002][1002];
+int GO(const int prev_idx, const int cur_idx)
+{
+    if (cur_idx == n)return 0;
+    auto& ref = memo[prev_idx + 1][cur_idx];
+    if (~ref)return ref;
+    int a = 0;
+    if (prev_idx == -1 || arr[prev_idx] < arr[cur_idx])
+    {
+        a = GO(cur_idx, cur_idx + 1) + 1;
+    }
+    return ref = max(a, GO(prev_idx, cur_idx + 1));
+}
+void Solve() noexcept
+{
+    cin >> n;
+    for (int i = 0; i < n; ++i)cin >> arr[i];
+    memset(memo, -1, sizeof(memo));
+    cout << GO(-1, 0);
+}
 int main()
 {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	int n; cin >> n;
-	for (int i = 1; i <= n; ++i)
-	{
-		cin >> arr[i];
-	}
-	dp[1] = 1;
-	for (int i = 2; i <= n; ++i)
-	{
-		int max_val = 0;
-		for (int j = i - 1; j >= 1; --j)
-		{
-			if (arr[j] < arr[i])
-			{
-				if (dp[j] > max_val)
-				{
-					max_val = dp[j];
-				}
-			}
-		}
-		dp[i] = max_val + 1;
-	}
-	cout << *max_element(dp + 1, dp + n + 1);
+    FastIO();
+    Solve();
 }

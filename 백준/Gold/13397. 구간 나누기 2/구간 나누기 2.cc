acff2f5d15bdc00ce85b8fe414759b24f5713db0
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-04-12 12:32:34 +0900

    [Gold IV] Title: 구간 나누기 2, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.cc
+++ 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.cc
@@ -8,37 +8,36 @@ using ull = unsigned long long;
 using pi = pair<int, int>;
 using pll = pair<ll, ll>;
 using pull = pair<ull, ull>;
-ull arr[100001];
-ull n, m;
-bool Check(const ull mid)
+int n, m;
+int arr[5001];
+bool Check(const ll mid)
 {
-    ull cnt = 1;
-    ull s = 0;
-    ull min_v = arr[0];
-    ull max_v = arr[0];
+    int cur_min = arr[0];
+    int cur_max = arr[0];
+    int cnt = 1;
     for (int i = 0; i < n; ++i)
     {
-        min_v = min(min_v, arr[i]);
-        max_v = max(max_v, arr[i]);
-        if (max_v - min_v > mid)
+        cur_min = min(cur_min, arr[i]);
+        cur_max = max(cur_max, arr[i]);
+        if (cur_max - cur_min > mid)
         {
             ++cnt;
-            min_v = arr[i];
-            max_v = arr[i];
+            if (cnt > m)return false;
+            cur_min = cur_max = arr[i];
         }
     }
-    return cnt <= m;
+    return true;
 }
 void Solve() noexcept
 {
     cin >> n >> m;
     for (int i = 0; i < n; ++i)cin >> arr[i];
-    ull low = 0;
-    ull high = INT32_MAX / 8;
-    ull ans = 0;
+    ll low = 0;
+    ll high = 10002;
+    ll ans = low;
     while (low < high)
     {
-        const auto mid = low + (high - low) / 2ULL;
+        const auto mid = low + (high - low) / 2LL;
         if (Check(mid))
         {
             ans = mid;

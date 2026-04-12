96901f49d23cb2d36ae66822e6270651337b1fb5
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-04-12 16:23:31 +0900

    [Gold V] Title: 평범한 배낭, Time: 64 ms, Memory: 41476 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,21 +1,36 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<int>(1e9) + 1;
+constexpr const int MIN_INF = -INF;
+using ll = long long;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using pull = pair<ull, ull>;
+int w[101];
+int v[101];
+int n, k;
+int memo[101][100001];
+int GO(const int idx, const int cap)
+{
+    if (cap < 0)return -INF;
+    if (cap == 0)return 0;
+    if (idx == n)return cap < 0 ? -INF : 0;
+    auto& ref = memo[idx][cap];
+    if (~ref)return ref;
+    return ref = max(GO(idx + 1, cap - w[idx]) + v[idx],
+               GO(idx + 1, cap));
+}
+void Solve() noexcept
+{
+    cin >> n >> k;
+    for (int i = 0; i < n; ++i)cin >> w[i] >> v[i];
+    memset(memo, -1, sizeof(memo));
+    cout << GO(0, k);
+}
 int main()
 {
-	int dp[100001]{};
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-	int n, m;
-	int res = 0;
-	cin >> n >> m;
-	for (int i = 0; i < n; ++i) {
-		int w, v;
-		cin >> w >> v;
-		for (int j = m; j >= w; --j) {
-			dp[j] = max(dp[j], dp[j - w] + v);
-			res = max(dp[j], res);
-		}
-	}
-	cout << res;
+    FastIO();
+    Solve();
 }

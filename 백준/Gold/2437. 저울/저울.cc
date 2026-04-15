9aea4b01392775890d6ddcebd345e3373d789ff2
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-04-15 10:06:47 +0900

    [Gold II] Title: 저울, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+constexpr const int INF = static_cast<int>(1e9) + 1;
+constexpr const int MIN_INF = -INF;
+using ll = long long;
+using ull = unsigned long long;
+using pi = pair<int, int>;
+using pll = pair<ll, ll>;
+using pull = pair<ull, ull>;
+int acc[1001];
+void Solve() noexcept
+{
+    int n; cin >> n;
+    vector<int> v; v.reserve(n);
+    int s = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        int x; cin >> x;
+        v.emplace_back(x);
+    }
+    sort(v.begin(), v.end());
+    // 1 1 2 3 6 7 30
+    int cur = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        if (v[i] <= cur + 1)
+        {
+            cur += v[i];
+        }
+        else
+        {
+            break;
+        }
+    }
+    cout << cur + 1;
+}
+int main()
+{
+    FastIO();
+    Solve();
+}

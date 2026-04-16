ea6d2bf2234e661d1112bbdaff848f615a464a03
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-04-17 08:14:25 +0900

    [Gold III] Title: 순회강연, Time: 0 ms, Memory: 2224 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.cc
+++ 백준/Gold/2109. 순회강연/순회강연.cc
@@ -1,35 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 void FastIO()noexcept { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
-constexpr const int INF = static_cast<int>(1e9);
+constexpr const int INF = static_cast<int>(1e9) + 1;
 constexpr const int MIN_INF = -INF;
 using ll = long long;
 using ull = unsigned long long;
 using pi = pair<int, int>;
 using pll = pair<ll, ll>;
 using pull = pair<ull, ull>;
-pi arr[10001];
-void Solve()noexcept
+void Solve() noexcept
 {
     int n; cin >> n;
+    vector<pi> v; v.reserve(n);
     for (int i = 0; i < n; ++i)
     {
-        cin >> arr[i].second >> arr[i].first;
+        int pay, day; cin >> pay >> day;
+        v.emplace_back(day, pay);
     }
-    sort(arr, arr + n);
+    sort(v.begin(), v.end());
     priority_queue<int, vector<int>, greater<int>> pq;
-    for (int i = 0; i < n; ++i)
+    ll s = 0;
+    for (const auto [day, pay] : v)
     {
-        pq.emplace(arr[i].second);
-        if (pq.size() > arr[i].first)pq.pop();
+        s += pay;
+        pq.emplace(pay);
+        if (pq.size() > day)
+        {
+            s -= pq.top();
+            pq.pop();
+        }
     }
-    int res = 0;
-    while (pq.size())
-    {
-        res += pq.top();
-        pq.pop();
-    }
-    cout << res;
+    cout << s;
 }
 int main()
 {

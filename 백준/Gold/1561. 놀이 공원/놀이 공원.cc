084efde22125b267949c631c73d26ce3929b1712
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-04-17 08:28:56 +0900

    [Gold I] Title: 놀이 공원, Time: 4 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Gold/1561. 놀이 공원/놀이 공원.cc
+++ 백준/Gold/1561. 놀이 공원/놀이 공원.cc
@@ -10,50 +10,44 @@ using pll = pair<ll, ll>;
 using pull = pair<ull, ull>;
 ll n, m;
 ll arr[10001];
-ll Cal(const ll t)
+ll Cal(const ll mid)
 {
-    ll s = 0;
-    for (int i = 0; i < m; ++i)
-    {
-        s += (t / arr[i]) + 1LL;
-    }
+    ll s = m;
+    for (int i = 0; i < m; ++i)s += mid / arr[i];
     return s;
 }
 void Solve() noexcept
 {
     cin >> n >> m;
+    for (int i = 0; i < m; ++i)cin >> arr[i];
     if (n <= m)
     {
         cout << n;
         return;
     }
-    for (int i = 0; i < m; ++i)cin >> arr[i];
-    ll low = 0;
-    ll high = INT32_MAX * 31LL;
+    ll left = 0;
+    ll right = INT32_MAX * 31LL;
     ll ans = 0;
-    while (low < high)
+    while (left < right)
     {
-        const auto mid = low + (high - low) / 2LL;
+        const auto mid = left + (right - left) / 2LL;
         const auto val = Cal(mid);
         if (n <= val)
         {
-            // n을 아득히 넘어가더라도 그럼 그냥 n만큼 태우면 그만, 
-            // 그래도 시간이 mid는걸린다는 사실엔 변함이없음
             ans = mid;
-            high = mid;
+            right = mid;
         }
         else
         {
-            low = mid + 1;
+            left = mid + 1;
         }
     }
-    const auto before = Cal(ans - 1); // 직전에 몇명태울수있었지?
-    auto diff_num = n - before;
+    const auto before = Cal(ans - 1);
+    auto nokori = n - before;
     for (int i = 0; i < m; ++i)
     {
-        if (ans % arr[i] != 0)continue; // 정확히 ans초에 끝나기 때문에 배수관계만 가능
-        --diff_num;
-        if (diff_num == 0)
+        if (ans % arr[i] != 0)continue;
+        if (--nokori == 0)
         {
             cout << i + 1;
             return;

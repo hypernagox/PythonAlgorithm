fc0fa67c97b9e6ebfb3eba28c95ee3bcc8e8fac7
hypernagox <128601833+hypernagox@users.noreply.github.com>
2026-04-19 13:22:32 +0900

    [Silver I] Title: 징검다리 건너기 (large), Time: 20 ms, Memory: 2348 KB -BaekjoonHub

--- 백준/Silver/22871. 징검다리 건너기 （large）/징검다리 건너기 （large）.cc
+++ 백준/Silver/22871. 징검다리 건너기 （large）/징검다리 건너기 （large）.cc
@@ -11,46 +11,26 @@ using pull = pair<ull, ull>;
 int n;
 int arr[5001];
 ll k;
-int memo[5001];
-bool GO(const int idx)
+ll memo[5001];
+ll GO(const int idx)
 {
-    if (idx == n)return 1;
-    if (idx == n - 1)return 1;
+    if (idx == n - 1)return 0;
     auto& ref = memo[idx];
     if (~ref)return ref;
+    ll res = INF;
     for (ll i = idx + 1; i < n; ++i)
     {
         const auto power = (i - idx) * (1LL + abs(arr[i] - arr[idx]));
-        if (power <= k && GO(i))
-        {
-            return ref = true;
-        }
+        res = min(res, max(power, GO(i)));
     }
-    return ref = false;
+    return ref = res;
 }
 void Solve() noexcept
 {
     cin >> n;
     for (int i = 0; i < n; ++i)cin >> arr[i];
-    ll low = 0;
-    ll high = INT32_MAX;
-    ll ans = 0;
-    while(low < high)
-    {
-        const auto mid = (low + high) / 2LL;
-        k = mid;
-        memset(memo, -1, sizeof(memo));
-        if (GO(0))
-        {
-            ans = mid;
-            high = mid;
-        }
-        else
-        {
-            low = mid + 1;
-        }
-    }
-    cout << ans;
+    memset(memo, -1, sizeof(memo));
+    cout << GO(0);
 }
 int main()
 {
